05573d2d8730ba34626c1b817a6617e67bf3d6de
Etienne Noel <etienne.noel@ima-tech.ca>
2020-12-23 13:29:31 -0500

    - Update.

---
+++ demo/projects/express/src/index.ts
@@ -0,0 +1,28 @@
+import {ExpressModule, RequestMapper, ResponseMapper} from "@pristine-ts/express";
+import {controller, HttpMethod, Kernel, route} from "@pristine-ts/core";
+import {DogsController} from "./controllers/dogs.controller";
+
+const express = require('express')
+const app = express()
+const port = 3000
+const kernel = new Kernel();
+
+function bootstrap () {
+    app.all('*', async (req, res) => {
+        const expressRequestMapper = kernel.container.resolve(RequestMapper);
+        const expressResponseMapper = kernel.container.resolve(ResponseMapper);
+
+        expressResponseMapper.reverseMap(await kernel.handleRequest(expressRequestMapper.map(req)), res);
+    })
+
+    app.listen(port, async () => {
+        await kernel.init({
+            importModules: [ExpressModule],
+            importServices: [DogsController],
+        })
+
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}
+
+bootstrap();
