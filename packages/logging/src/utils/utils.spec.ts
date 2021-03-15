d2e679f5945de74089f14c9e25311ff62e6780c6
mathieugh <mathieu.giroux-huppe@polymtl.ca>
2021-03-15 14:21:41 -0400

    Added tests for flat types

--- packages/logging/src/utils/utils.spec.ts
+++ packages/logging/src/utils/utils.spec.ts
@@ -20,4 +20,41 @@ describe("Utils", () => {
         expect(Utils.truncate(object, 4)).toEqual({bonjour:{allo:{bye:"byebye", date: new Date(1615830493000)}}});
     });
 
+    it("should consider string as a flat type", async () => {
+
+        const object = {
+            bonjour: "allo",
+        };
+
+        expect(Utils.truncate(object, 1)).toEqual({bonjour:"allo"});
+    });
+
+    it("should consider a number as a flat type", async () => {
+
+        const object = {
+            bonjour: 42,
+        };
+
+        expect(Utils.truncate(object, 1)).toEqual({bonjour:42});
+    });
+
+
+    it("should consider a boolean as a flat type", async () => {
+
+        const object = {
+            bonjour: true,
+        };
+
+        expect(Utils.truncate(object, 1)).toEqual({bonjour:true});
+    });
+
+    it("should consider a date as a flat type", async () => {
+
+        const object = {
+            bonjour: new Date(1615830493000),
+        };
+
+        expect(Utils.truncate(object, 1)).toEqual({bonjour:new Date(1615830493000)});
+    });
+
 });
