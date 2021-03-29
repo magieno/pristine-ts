193e2577717ac5afc183ebac3fc8affbd0033153
mathieugh <mathieu.girouxh@gmail.com>
2021-03-29 10:58:03 -0400

    Added the sns event parser

--- packages/aws/src/event-parsers/s3.event-parser.spec.ts
+++ packages/aws/src/event-parsers/s3.event-parser.spec.ts
@@ -54,37 +54,37 @@ describe("S3 event parser", () => {
         const s3Event: Event<S3EventPayload> = {
             type: S3EventType.ObjectCreatedPut,
             payload: {
-                "eventVersion":"2.1",
-                "eventSource":"aws:s3",
-                "awsRegion":"us-west-2",
-                "eventTime": new Date("1970-01-01T00:00:00.000Z"),
-                "eventName":"ObjectCreated:Put",
-                "userIdentity":{
-                    "principalId":"AIDAJDPLRKLG7UEXAMPLE"
+                eventVersion:"2.1",
+                eventSource:"aws:s3",
+                awsRegion:"us-west-2",
+                eventTime: new Date("1970-01-01T00:00:00.000Z"),
+                eventName:"ObjectCreated:Put",
+                userIdentity:{
+                    principalId:"AIDAJDPLRKLG7UEXAMPLE"
                 },
-                "requestParameters":{
-                    "sourceIPAddress":"127.0.0.1"
+                requestParameters:{
+                    sourceIPAddress:"127.0.0.1"
                 },
-                "responseElements":{
+                responseElements:{
                     "x-amz-request-id":"C3D13FE58DE4C810",
                     "x-amz-id-2":"FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
                 },
-                "s3":{
-                    "s3SchemaVersion":"1.0",
-                    "configurationId":"testConfigRule",
-                    "bucket":{
-                        "name":"mybucket",
-                        "ownerIdentity":{
-                            "principalId":"A3NL1KOZZKExample"
+                s3:{
+                    s3SchemaVersion:"1.0",
+                    configurationId:"testConfigRule",
+                    bucket:{
+                        name:"mybucket",
+                        ownerIdentity:{
+                            principalId:"A3NL1KOZZKExample"
                         },
-                        "arn":"arn:aws:s3:::mybucket"
+                        arn:"arn:aws:s3:::mybucket"
                     },
-                    "object":{
-                        "key":"HappyFace.jpg",
-                        "size":1024,
-                        "eTag":"d41d8cd98f00b204e9800998ecf8427e",
-                        "versionId":"096fKKXTRTtl3on89fVO.nfljtsv6qko",
-                        "sequencer":"0055AED6DCD90281E5"
+                    object:{
+                        key:"HappyFace.jpg",
+                        size:1024,
+                        eTag:"d41d8cd98f00b204e9800998ecf8427e",
+                        versionId:"096fKKXTRTtl3on89fVO.nfljtsv6qko",
+                        sequencer:"0055AED6DCD90281E5"
                     }
                 }
             }
