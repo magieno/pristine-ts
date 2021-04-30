300fa9b47daf6f057ddb542cf84e7451726ce5f4
mathieugh <mathieu.girouxh@gmail.com>
2021-04-30 19:13:18 -0400

    Initial cognito commit

---
+++ packages/cognito/src/cognito.module.keyname.ts
@@ -0,0 +1,1 @@
+export const CognitoModuleKeyname: string = "pristine.cognito";
---
+++ packages/cognito/src/cognito.module.ts
@@ -0,0 +1,30 @@
+import {ModuleInterface, ServiceDefinitionTagEnum} from "@pristine-ts/common";
+import {CognitoModuleKeyname} from "./cognito.module.keyname";
+
+export * from "./authenticators/authenticators";
+export * from "./clients/clients";
+export * from "./interfaces/interfaces";
+
+export const CognitoModule: ModuleInterface = {
+    keyname: CognitoModuleKeyname,
+    configurationDefinitions: [
+        {
+            parameterName: CognitoModuleKeyname + ".region",
+            isRequired: false,
+            defaultValue: "us-east-1",
+        },
+        {
+            parameterName: CognitoModuleKeyname + ".poolId",
+            isRequired: true,
+        }
+    ],
+
+    importServices: [],
+
+    /**
+     * This property allows you to custom register specific services. For example, you can assign a tag or use a factory
+     * to instantiate a specific class.
+     */
+    providerRegistrations: [
+    ],
+}
