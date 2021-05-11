45cf33747006d4e33318aac46cff4d168ed6f493
Etienne Noel <etiennenoel@gmail.com>
2021-05-11 16:02:42 -0400

    Update.

--- packages/networking/src/router.ts
+++ packages/networking/src/router.ts
@@ -1,5 +1,4 @@
 import {DependencyContainer, inject, singleton} from "tsyringe";
-import {HttpMethod} from "./enums/http-method.enum";
 import {Request} from "./models/request";
 import {Response} from "./models/response";
 import {UrlUtil} from "./utils/url.util";
@@ -12,8 +11,7 @@ import {MethodRouterNode} from "./nodes/method-router.node";
 import {ForbiddenHttpError} from "./errors/forbidden.http-error";
 import {ControllerMethodParameterDecoratorResolver} from "./resolvers/controller-method-parameter-decorator.resolver";
 import Url from 'url-parse';
-import {HttpMethod, IdentityInterface} from "@pristine-ts/common";
-import {IdentityInterface, ServiceDefinitionTagEnum} from "@pristine-ts/common";
+import {HttpMethod, IdentityInterface, ServiceDefinitionTagEnum} from "@pristine-ts/common";
 import {AuthenticationManagerInterface, AuthorizerManagerInterface} from "@pristine-ts/security";
 import {RouterResponseEnricherInterface} from "./interfaces/router-response-enricher.interface";
 
