d0a4e57992dcd60b45310a4c5a2ab770dc8354a0
Etienne Noel <etiennenoel@gmail.com>
2021-05-11 15:56:03 -0400

    - Updated the client.

--- packages/http/src/clients/http.client.ts
+++ packages/http/src/clients/http.client.ts
@@ -2,7 +2,7 @@ import {injectable} from "tsyringe";
 import {HttpClientInterface} from "../interfaces/http-client.interface";
 import {HttpRequestInterface} from "../interfaces/http-request.interface";
 import {HttpResponseInterface} from "../interfaces/http-response.interface";
-import {IncomingMessage, request as httpRequest} from "http";
+import {IncomingMessage, request as httpRequest, RequestOptions} from "http";
 import {request as httpsRequest} from "https";
 import Url from 'url-parse';
 import {InvalidHttpRequestProtocolError} from "../errors/invalid-http-request-protocol.error";
@@ -12,30 +12,30 @@ export class HttpClient implements HttpClientInterface {
     request(request: HttpRequestInterface): Promise<HttpResponseInterface> {
         return new Promise((resolve, reject) => {
             const url = new Url(request.url, true);
-            const options = {
+            const options: RequestOptions = {
                 host: url.hostname,
                 path: url.pathname + "?" + Object.keys(url.query).map(key => key + "=" + url.query).join("&"),
                 method: request.httpMethod,
                 headers: request.headers,
-                port: 80,
+                port: url.port,
             }
 
-            const defaultResponeStatus = 200; // todo: decide if that's how we should proceed
+            const defaultResponseStatus = 200; // todo: decide if that's how we should proceed
             const response: HttpResponseInterface = {
                 request,
-                status: defaultResponeStatus,
+                status: defaultResponseStatus,
             };
 
             const callback = (res: IncomingMessage) => {
                 response.headers = res.headers as {
                     [key: string]: string;
                 };
-                response.status = res.statusCode ?? defaultResponeStatus;
+                response.status = res.statusCode ?? defaultResponseStatus;
 
-                let body;
+                let body = '';
 
                 res.on('data', chunk => {
-                    response.body += chunk;
+                    body = body + "" + chunk;
                 });
 
                 res.on('error', error => {
@@ -43,24 +43,22 @@ export class HttpClient implements HttpClientInterface {
                 })
 
                 res.on('end', () => {
+                    response.body = body;
                     return resolve(response);
                 });
             }
 
-            if( (url.slashes && url.protocol === "http://") ||
-                (url.slashes === false && url.protocol === "http:")) {
-
+            if(url.protocol === "http://" || url.protocol === "http:" || url.protocol === "http") {
                 const req = httpRequest(options, callback);
 
                 if(request.body) {
                     req.write(request.body);
                 }
 
-                req.end();
+                return req.end();
             }
-            else if( (url.slashes && url.protocol === "https://") ||
-                (url.slashes === false && url.protocol === "https:")) {
-                options.port = 443;
+            else if( url.protocol === "https://" || url.protocol === "https:" || url.protocol === "https") {
+                options.port = options.port ?? 443;
 
                 const req = httpsRequest(options, callback);
 
@@ -68,7 +66,7 @@ export class HttpClient implements HttpClientInterface {
                     req.write(request.body);
                 }
 
-                req.end();
+                return req.end();
             }
 
             return reject(new InvalidHttpRequestProtocolError("The protocol for the HttpRequest is invalid. It must be 'http' or 'https', you provided: '" + url.protocol + "'"));
--- tests/e2e/scenarios/http-client.scenario.e2e.ts
+++ tests/e2e/scenarios/http-client.scenario.e2e.ts
@@ -1,33 +1,137 @@
-import {request as httpRequest, createServer} from "http";
-import {request as httpsRequest} from "https";
+import "reflect-metadata"
+import {createServer} from "http";
+import {createServer as createServerHttps, globalAgent} from "https";
 import {HttpClient} from "@pristine-ts/http";
+import {HttpMethod} from "@pristine-ts/common";
+import {readFileSync} from "fs";
 
 describe("Http Client", () => {
     it("should make an Http Request if the protocol is http", async () => {
         const host = 'localhost';
-        const port = 8000;
+        const port = 0;
 
         await new Promise<void>(resolve => {
-            const server = createServer((req, res) => {
-                res.writeHead(200);
-                res.end("My first server!");
-            });
+            const server = createServer();
 
-            server.listen(port, host, () => {
-                // Make an HTTP call using the httpClient
-                const httpClient = new HttpClient()
-                const response = httpClient.request({
+            server.on("request", (req, res) => {
+                let data = '';
 
+                req.on('data', chunk => {
+                    data += chunk;
                 });
 
+                req.on('end', () => {
+                    res.writeHead(200);
+                    const responseBody = {
+                        headers: req.headers,
+                        method: req.method,
+                        body: data,
+                    };
+
+                    res.write(JSON.stringify(responseBody))
+                    res.end();
+                });
+
+            })
+
+            server.listen(port, host, async () => {
+                // Make an HTTP call using the httpClient
+                const httpClient = new HttpClient()
+                const requestbody = JSON.stringify({
+                    keyname: "Value",
+                });
+                const response = await httpClient.request({
+                    // @ts-ignore
+                    url: "http://localhost:" + server.address()!.port,
+                    httpMethod: HttpMethod.Put,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Token": "AuthorizationToken",
+                    },
+                    body: requestbody,
+                });
+
+                expect(response.status).toBe(200)
+
+                const parsedBody = JSON.parse(response.body);
+
+                expect(parsedBody.method).toBe(HttpMethod.Put);
+                expect(parsedBody.body).toBe(requestbody);
+                expect(parsedBody.headers["content-type"]).toBe("application/json");
+                expect(parsedBody.headers["x-token"]).toBe("AuthorizationToken");
+
+                return resolve();
+            });
+
+        })
+
+        expect.assertions(5)
+    })
+
+    it("should make an Https Request if the protocol is https", async () => {
+        globalAgent.options.rejectUnauthorized = false
+
+        const host = 'localhost';
+        const port = 0;
+
+        await new Promise<void>(resolve => {
+            const server = createServerHttps({
+                key: readFileSync(__dirname + '/../files/localhost.key'),
+                cert: readFileSync(__dirname + '/../files/localhost.crt'),
+                rejectUnauthorized: false,
+            });
+
+            server.on("request", (req, res) => {
+                let data = '';
+
+                req.on('data', chunk => {
+                    data += chunk;
+                });
+
+                req.on('end', () => {
+                    res.writeHead(200);
+                    const responseBody = {
+                        headers: req.headers,
+                        method: req.method,
+                        body: data,
+                    };
+
+                    res.write(JSON.stringify(responseBody))
+                    res.end();
+                });
+
+            })
+
+            server.listen(port, host, async () => {
+                // Make an HTTP call using the httpClient
+                const httpClient = new HttpClient()
+                const requestbody = JSON.stringify({
+                    keyname: "Value",
+                });
+                const response = await httpClient.request({
+                    // @ts-ignore
+                    url: "https://localhost:" + server.address()!.port,
+                    httpMethod: HttpMethod.Put,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Token": "AuthorizationToken",
+                    },
+                    body: requestbody,
+                });
+
+                expect(response.status).toBe(200)
+
+                const parsedBody = JSON.parse(response.body);
+
+                expect(parsedBody.method).toBe(HttpMethod.Put);
+                expect(parsedBody.body).toBe(requestbody);
+                expect(parsedBody.headers["content-type"]).toBe("application/json");
+                expect(parsedBody.headers["x-token"]).toBe("AuthorizationToken");
 
                 return resolve();
             });
         })
 
-    })
-
-    it("should make an Https Request if the protocol is https", () => {
-
+        expect.assertions(5)
     })
 })
