2455d6f11a9c7e65631fe666c2e81601de5b0d80
Etienne Noël <etienne.noel@appdirect.com>
2021-05-13 21:53:47 -0400

    updated the interface.

--- packages/telemetry/src/interfaces/tracing-manager.interface.ts
+++ packages/telemetry/src/interfaces/tracing-manager.interface.ts
@@ -4,5 +4,9 @@ import {SpanKeynameEnum} from "../enums/span-keyname.enum";
 export interface TracingManagerInterface {
     startTracing(spanRootKeyname?: string, traceId?: string, context?: any);
 
+    endTrace();
+
+    startSpan(keyname: string, parentId?: string, context?: any);
+
     endSpan(span: Span);
 }
