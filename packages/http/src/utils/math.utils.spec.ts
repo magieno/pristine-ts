1d26e60bbf18ad5167892b01699d2fe17824e948
Etienne Noel <etiennenoel@gmail.com>
2021-05-19 16:20:28 -0400

    Update.

--- packages/http/src/utils/math.utils.spec.ts
+++ packages/http/src/utils/math.utils.spec.ts
@@ -2,12 +2,19 @@ import {MathUtils} from "./math.utils";
 
 describe("Math Utils", () => {
     it("should return the proper expected exponential backoff", () => {
-        const a1 = MathUtils.exponentialBackoffWithJitter(0);
-        const a2 = MathUtils.exponentialBackoffWithJitter(1);
-        const a = MathUtils.exponentialBackoffWithJitter(2);
-        const b = MathUtils.exponentialBackoffWithJitter(3);
-        const c = MathUtils.exponentialBackoffWithJitter(4);
-        const d = MathUtils.exponentialBackoffWithJitter(5);
-        const e = MathUtils.exponentialBackoffWithJitter(6);
+        const a = MathUtils.exponentialBackoffWithJitter(0);
+        const b = MathUtils.exponentialBackoffWithJitter(1);
+        const c = MathUtils.exponentialBackoffWithJitter(2);
+        const d = MathUtils.exponentialBackoffWithJitter(3);
+        const e = MathUtils.exponentialBackoffWithJitter(4);
+        const f = MathUtils.exponentialBackoffWithJitter(5);
+        const g = MathUtils.exponentialBackoffWithJitter(6);
+
+        expect(a).toBeLessThan(b);
+        expect(b).toBeLessThan(c);
+        expect(c).toBeLessThan(d);
+        expect(d).toBeLessThan(e);
+        expect(e).toBeLessThan(f);
+        expect(f).toBeLessThan(g);
     })
 })
