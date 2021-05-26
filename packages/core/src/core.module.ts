af80f647a82db5fd31790554358e184a51a27a58
Etienne Noël <etienne.noel@appdirect.com>
2021-05-25 21:58:25 -0400

    Starting to embed the tracing directly into the Kernel.

--- packages/core/src/core.module.ts
+++ packages/core/src/core.module.ts
@@ -1,18 +1,21 @@
 import {ModuleInterface} from "@pristine-ts/common";
 import {NetworkingModule} from "@pristine-ts/networking";
 import {CoreModuleKeyname} from "./core.module.keyname";
+import {TelemetryModule} from "@pristine-ts/telemetry";
 
 export const CoreModule: ModuleInterface =  {
     keyname: CoreModuleKeyname,
     importServices: [],
     importModules: [
-        NetworkingModule
+        NetworkingModule,
+        TelemetryModule,
     ],
     providerRegistrations: [],
 }
 
 export * from "./kernel";
 
+export * from "./enums/enums";
 export * from "./errors/errors";
 export * from "./interceptors/interceptors";
 export * from "./interfaces/interfaces";
---
+++ packages/core/src/enums/span-keynames.enum.ts
@@ -0,0 +1,4 @@
+export enum SpanKeynamesEnum {
+    Event = "EVENT",
+    Request = "REQUEST",
+}
