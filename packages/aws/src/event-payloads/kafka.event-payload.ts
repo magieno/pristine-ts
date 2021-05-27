ec22c665227cfd70e565b2a7c05cbeac075b4c1c
mathieugh <mathieu.girouxh@gmail.com>
2021-05-27 09:13:11 -0400

    - Remove region

--- packages/aws/src/event-payloads/kafka.event-payload.ts
+++ packages/aws/src/event-payloads/kafka.event-payload.ts
@@ -3,6 +3,5 @@ import {KafkaTopicModel} from "../models/kafka-topic.model";
 export class KafkaEventPayload {
     eventSource: string;
     eventSourceArn: string;
-    awsRegion: string;
     topics: KafkaTopicModel[];
 }
