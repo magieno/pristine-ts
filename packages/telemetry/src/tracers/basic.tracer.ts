944bff077ba99a3b3a4c172d36c37074c48fbda1
mathieugh <mathieu.girouxh@gmail.com>
2021-05-27 13:40:51 -0400

    - Add comment

--- packages/telemetry/src/tracers/basic.tracer.ts
+++ packages/telemetry/src/tracers/basic.tracer.ts
@@ -3,7 +3,9 @@ import {TracerInterface} from "@pristine-ts/telemetry";
 import { ServiceDefinitionTagEnum, tag} from "@pristine-ts/common";
 import {Readable} from "stream";
 
-
+/**
+ * We need this to have at least one tracer so the @injectAll(ServiceDefinitionTagEnum.Tracer) does not fail
+ */
 @tag(ServiceDefinitionTagEnum.Tracer)
 @injectable()
 export class XrayTracer implements TracerInterface {
