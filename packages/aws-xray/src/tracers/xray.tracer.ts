1c902590dbd0b8f4aa9c626a2b0fd673749a97bd
Etienne Noel <etiennenoel@gmail.com>
2021-05-27 21:12:17 -0400

    Trying something elese.

--- packages/aws-xray/src/tracers/xray.tracer.ts
+++ packages/aws-xray/src/tracers/xray.tracer.ts
@@ -63,9 +63,8 @@ export class XrayTracer implements TracerInterface{
     }
 
     private traceStarted(trace: Trace): Segment {
-        const segment = new AWSXRay.Segment(trace.rootSpan.keyname);
-        segment.id = trace.id;
-        segment.trace_id = trace.id;
+        const segment = AWSXRay.getSegment() as Segment;
+        segment.addMetadata("pristine_trace_id", trace.id)
 
         if(trace.rootSpan.context) {
             Object.keys(trace.rootSpan.context).forEach(key => {
