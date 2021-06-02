912240ace3515783ed5d1597b45b16334e7ed8a8
mathieugh <mathieu.girouxh@gmail.com>
2021-06-02 15:38:09 -0400

    - Added redis client

--- packages/redis/src/redis.module.ts
+++ packages/redis/src/redis.module.ts
@@ -1,11 +1,40 @@
 import {ModuleInterface} from "@pristine-ts/common";
 import {LoggingModule} from "@pristine-ts/logging";
 import {RedisModuleKeyname} from "./redis.module.keyname";
+import { EnvironmentVariableResolver, NumberResolver} from "@pristine-ts/configuration";
+
+export * from "./clients/clients";
+export * from "./errors/errors";
 
 export const RedisModule: ModuleInterface = {
     keyname: RedisModuleKeyname,
     importModules: [
         LoggingModule,
     ],
-    providerRegistrations: []
+    providerRegistrations: [],
+    configurationDefinitions: [
+        {
+            parameterName: RedisModuleKeyname + ".host",
+            isRequired: true,
+            defaultResolvers: [
+                new EnvironmentVariableResolver("PRISTINE_REDIS_HOST"),
+            ]
+        },
+        {
+            parameterName: RedisModuleKeyname + ".port",
+            isRequired: false,
+            defaultValue: 6379,
+            defaultResolvers: [
+                new NumberResolver(new EnvironmentVariableResolver("PRISTINE_REDIS_PORT")),
+            ]
+        },
+        {
+            parameterName: RedisModuleKeyname + ".namespace",
+            isRequired: true,
+            defaultResolvers: [
+                new EnvironmentVariableResolver("PRISTINE_REDIS_NAMESPACE"),
+            ]
+        }
+    ]
+
 }
