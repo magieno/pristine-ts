a05180a947dc5eb43a0240d163f5af542c8ee071
mathieugh <mathieu.girouxh@gmail.com>
2021-06-08 14:31:55 -0400

    - Created authenticator

---
+++ packages/auth0/src/authenticators/auth0.authenticator.spec.ts
@@ -0,0 +1,257 @@
+import "reflect-metadata"
+import {Auth0Authenticator} from "./auth0.authenticator";
+import {HttpMethod, RequestInterface} from "@pristine-ts/common";
+import * as jwt from "jsonwebtoken";
+import {HttpClientInterface, HttpRequestInterface, HttpResponseInterface} from "@pristine-ts/http";
+import {LogHandlerInterface} from "@pristine-ts/logging";
+
+const logHandlerMock: LogHandlerInterface = {
+    debug(message: string, extra?: any) {
+    },
+    info(message: string, extra?: any) {
+    },
+    error(message: string, extra?: any) {
+    }
+    ,critical(message: string, extra?: any) {
+    },
+    warning(message: string, extra?: any) {
+    },
+}
+
+const privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
+    "MIIBOQIBAAJAXmWi+JMuW8v5Ng5sDso+H6wl+i9u7lwMxJrZ+j0VQNEh4E7EwHQM\n" +
+    "PEnPJkTO3cKg6lDwKZ4HX/5BsAQ8ST5nywIDAQABAkAwU7hrqm5BcNvVYOzRZZkI\n" +
+    "fgmzXDVesqGWxfByvHL1C30kB6mvPC6K9iBuaN0MrwR2YIR+LPDmKbFC3jkVzkwB\n" +
+    "AiEAn1BRnlbHKfv7fArL+TX/mRy8jJ4KjuQfMWATt7QnGCsCIQCXr5Iao/wCDzHL\n" +
+    "E5aI00CE5J1W+yU9fvbvQUtA63d+4QIhAJcusUWBNB8zcMOVu1sTUysJiiQnFf3j\n" +
+    "nXZNpmh+HVthAiAU1magyEQ3WDUD68XzN+oWF33R1CByiT2M8pBO88nvoQIgLVZo\n" +
+    "5AsRaiJAQou6tjgUqHJrP4uv+Q5kKncGzN5aEy8=\n" +
+    "-----END RSA PRIVATE KEY-----\n";
+
+const publicKey1 = "-----BEGIN PUBLIC KEY-----\n" +
+    "MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAXmWi+JMuW8v5Ng5sDso+H6wl+i9u7lwM\n" +
+    "xJrZ+j0VQNEh4E7EwHQMPEnPJkTO3cKg6lDwKZ4HX/5BsAQ8ST5nywIDAQAB\n" +
+    "-----END PUBLIC KEY-----\n";
+
+const tokenHeader = {
+    alg: "RS256",
+    kid: "687dfb71-7ce9-42b5-b77c-c39ac2dfd21e",
+    typ: "JWT"
+};
+
+const publicKeys = {
+    "keys": [{
+        "alg": "RS256",
+        "kty":"RSA",
+        "e":"AQAB",
+        "kid":"687dfb71-7ce9-42b5-b77c-c39ac2dfd21e",
+        "n":"XmWi-JMuW8v5Ng5sDso-H6wl-i9u7lwMxJrZ-j0VQNEh4E7EwHQMPEnPJkTO3cKg6lDwKZ4HX_5BsAQ8ST5nyw"
+    }, {
+        "alg": "RS256",
+        "e": "AQAB",
+        "kid": "fgjhlkhjlkhexample=",
+        "kty": "RSA",
+        "n": "sgjhlk6jp98ugp98up34hpexample",
+        "use": "sig"
+    }]
+}
+let payload;
+
+export class MockHttpClient implements HttpClientInterface {
+
+    request(request: HttpRequestInterface): Promise<HttpResponseInterface> {
+        return Promise.resolve({
+            body: publicKeys,
+            request: {
+                httpMethod: HttpMethod.Get,
+                headers: {},
+                url: "",
+            },
+            headers: {},
+            status: 200,
+        });
+    }
+}
+
+describe("Auth0 authenticator ", () => {
+
+    beforeEach(() => {
+        payload = {
+            "sub": "aaaaaaaa-bbbb-cccc-dddd-example",
+            "aud": "xxxxxxxxxxxxexample",
+            "email_verified": true,
+            "token_use": "access",
+            "auth_time": 1500009400,
+            "iss": "https://auth0.com/",
+            "exp": (Date.now() + 3600000)/1000,
+            "given_name": "Anaya",
+            "iat": 1500009400,
+            "email": "anaya@example.com"
+        }
+    });
+
+    it("should get auth0 issuer", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        expect(auth0Authenticator["getAuth0Issuer"]()).toBe("https://auth0.com/");
+    });
+
+    it("should get url", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        expect(auth0Authenticator["getPublicKeyUrl"]()).toBe("https://auth0.com/.well-known/jwks.json");
+    });
+
+    it("should get pems", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        expect(await auth0Authenticator["getPems"]()).toEqual({
+            "687dfb71-7ce9-42b5-b77c-c39ac2dfd21e": publicKey1,
+            "fgjhlkhjlkhexample=": "-----BEGIN PUBLIC KEY-----\nMDEwDQYJKoZIhvcNAQEBBQADIAAwHQIWALII4ZZOo6ffLoKffLqd+IaXsWpqZQID\nAQAB\n-----END PUBLIC KEY-----\n"
+        });
+    });
+
+    it("should validateRequestAndReturnToken", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: {
+                "Authorization": "Bearer " + "token",
+            }
+        }
+        expect(auth0Authenticator["validateRequestAndReturnToken"](request)).toBe("token");
+    });
+
+    it("should not validateRequestAndReturnToken if not headers", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: undefined
+        }
+        expect(() => auth0Authenticator["validateRequestAndReturnToken"](request)).toThrow(new Error("The Authorization header wasn't found in the Request."));
+    });
+
+    it("should not validateRequestAndReturnToken if no authorization header", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: {
+                hello: "string"
+            }
+        }
+        expect(() => auth0Authenticator["validateRequestAndReturnToken"](request)).toThrow(new Error("The Authorization header wasn't found in the Request."));
+    });
+
+    it("should not validateRequestAndReturnToken if authorization header undefined", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: {
+                // @ts-ignore
+                "Authorization": undefined
+            }
+        }
+        expect(() => auth0Authenticator["validateRequestAndReturnToken"](request)).toThrow(new Error("The Authorization header wasn't found in the Request."));
+    });
+
+    it("should not validateRequestAndReturnToken if authorization header does not start with Bearer", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: {
+                // @ts-ignore
+                "Authorization": "token"
+            }
+        }
+        expect(() => auth0Authenticator["validateRequestAndReturnToken"](request)).toThrow(new Error("The value in Authorization header doesn't start with 'Bearer '"));
+    });
+
+    it("should getAndVerifyClaims", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+        expect(auth0Authenticator["getAndVerifyClaims"](token, publicKey1)).toEqual(payload);
+    });
+
+    it("should not getAndVerifyClaims if expired", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        payload.exp = 1500000;
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+        expect(() => auth0Authenticator["getAndVerifyClaims"](token, publicKey1)).toThrow(new Error("Invalid jwt: jwt expired"));
+    });
+
+    it("should not getAndVerifyClaims if auth time after", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        payload.auth_time = 1500000000000;
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+        expect(() => auth0Authenticator["getAndVerifyClaims"](token, publicKey1)).toThrow(new Error('Claim is expired or invalid'));
+    });
+
+    it("should not getAndVerifyClaims if issuer different", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        payload.iss = "issuer";
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+        expect(() => auth0Authenticator["getAndVerifyClaims"](token, publicKey1)).toThrow(new Error('Claim issuer is invalid'));
+    });
+
+    it("should getKeyFromToken", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256', keyid: "687dfb71-7ce9-42b5-b77c-c39ac2dfd21e"});
+        const pems = {
+            "687dfb71-7ce9-42b5-b77c-c39ac2dfd21e": publicKey1,
+            "fgjhlkhjlkhexample=": "-----BEGIN PUBLIC KEY-----\nMDEwDQYJKoZIhvcNAQEBBQADIAAwHQIWALII4ZZOo6ffLoKffLqd+IaXsWpqZQID\nAQAB\n-----END PUBLIC KEY-----\n"
+        }
+        expect(auth0Authenticator["getKeyFromToken"](token, pems)).toBe(publicKey1);
+    });
+
+    it("should not getKeyFromToken if unknow kid", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256', keyid: "hello"});
+        const pems = {
+            "687dfb71-7ce9-42b5-b77c-c39ac2dfd21e": publicKey1,
+            "fgjhlkhjlkhexample=": "-----BEGIN PUBLIC KEY-----\nMDEwDQYJKoZIhvcNAQEBBQADIAAwHQIWALII4ZZOo6ffLoKffLqd+IaXsWpqZQID\nAQAB\n-----END PUBLIC KEY-----\n"
+        }
+        expect(() => auth0Authenticator["getKeyFromToken"](token, pems)).toThrow('Claim made for unknown kid');
+    });
+
+    it("should getTokenHeader", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        const token = jwt.sign(payload, privateKey, { algorithm: 'RS256', keyid: tokenHeader.kid});
+        expect(auth0Authenticator["getTokenHeader"](token)).toEqual(tokenHeader);
+    });
+
+    it("should not getTokenHeader with invalid token", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+        const token = "hello";
+        expect(() => auth0Authenticator["getTokenHeader"](token)).toThrow('Token is invalid');
+    });
+
+    it("should authenticate", async () => {
+        const auth0Authenticator = new Auth0Authenticator("auth0.com", new MockHttpClient(), logHandlerMock);
+
+        const request: RequestInterface = {
+            body: {},
+            url: "",
+            httpMethod: HttpMethod.Get,
+            headers: {
+                // @ts-ignore
+                "Authorization": "Bearer " + jwt.sign(payload, privateKey, { algorithm: 'RS256', keyid: tokenHeader.kid})
+            }
+        }
+        expect(await auth0Authenticator["authenticate"](request)).toEqual({
+            id: payload.sub,
+            claims: payload
+        });
+    });
+});
---
+++ packages/auth0/src/authenticators/auth0.authenticator.ts
@@ -0,0 +1,142 @@
+import {inject, injectable, singleton} from "tsyringe";
+import * as jwt from "jsonwebtoken";
+import {HttpMethod, IdentityInterface, RequestInterface} from "@pristine-ts/common";
+import {TokenHeaderInterface} from "../interfaces/token-header.interface";
+import {ClaimInterface} from "../interfaces/claim.interface";
+import {AuthenticatorInterface} from "@pristine-ts/security";
+import {HttpClientInterface, ResponseTypeEnum} from "@pristine-ts/http";
+import {LogHandlerInterface} from "@pristine-ts/logging";
+import jwkToBuffer from "jwk-to-pem";
+import {Auth0ModuleKeyname} from "../auth0.module.keyname";
+
+
+@singleton()
+@injectable()
+export class Auth0Authenticator implements AuthenticatorInterface{
+
+    private cachedPems;
+    private auth0Issuer;
+    private publicKeyUrl;
+    private context;
+
+    constructor(@inject(`%${Auth0ModuleKeyname}.domain%`) private readonly domain: string,
+                @inject("HttpClientInterface") private readonly httpClient: HttpClientInterface,
+                @inject("LogHandlerInterface") private readonly logHandler: LogHandlerInterface,
+                ) {
+        this.auth0Issuer = this.getAuth0Issuer();
+        this.publicKeyUrl = this.getPublicKeyUrl();
+    }
+
+    setContext(context: any): Promise<void> {
+        this.context = context;
+        return Promise.resolve();
+    }
+
+    async authenticate(request: RequestInterface): Promise<IdentityInterface> {
+        this.cachedPems = this.cachedPems ?? await this.getPems();
+        const token = this.validateRequestAndReturnToken(request);
+        const key = this.getKeyFromToken(token, this.cachedPems);
+
+        const claim = this.getAndVerifyClaims(token, key);
+
+        this.logHandler.debug("Claim confirmed", {
+            claim,
+        });
+
+        return {
+            id: claim.sub,
+            claims: claim
+        }
+    }
+
+    private getAuth0Issuer(): string {
+        return "https://" + this.domain + "/";
+    }
+
+    private getPublicKeyUrl(): string {
+        return this.auth0Issuer + ".well-known/jwks.json";
+    }
+
+    private async getPems() {
+        const publicKeysResponse = await this.httpClient.request({
+            httpMethod: HttpMethod.Get,
+            url: this.publicKeyUrl,
+        }, {
+            responseType: ResponseTypeEnum.Json,
+        });
+
+        const publicKeys = publicKeysResponse.body;
+
+        const pems: {[key: string]: string} = publicKeys.keys.reduce((agg, current) => {
+            agg[current.kid] = jwkToBuffer(current);
+            return agg;
+        }, {} as {[key: string]: string});
+
+        return pems;
+    }
+
+    // todo: this is a copy from jwt manager should we put that somewhere common ?
+    private validateRequestAndReturnToken(request: RequestInterface): string {
+        if (request.headers === undefined || (request.headers.hasOwnProperty("Authorization") === false && request.headers.hasOwnProperty("authorization") === false)) {
+            throw new Error("The Authorization header wasn't found in the Request.");
+            // throw new MissingAuthorizationHeaderError("The Authorization header wasn't found in the Request.");
+        }
+
+        const authorizationHeader = request.headers.Authorization ?? request.headers.authorization;
+
+        if (authorizationHeader === undefined) {
+            throw new Error("The Authorization header wasn't found in the Request.");
+
+            // throw new MissingAuthorizationHeaderError("The Authorization header wasn't found in the Request.");
+        }
+
+        if (authorizationHeader.startsWith("Bearer ") === false) {
+            throw new Error("The value in Authorization header doesn't start with 'Bearer '");
+
+            // throw new InvalidAuthorizationHeaderError("The value in Authorization header doesn't start with 'Bearer '")
+        }
+
+        return authorizationHeader.substr(7, authorizationHeader.length);
+    }
+
+    private getAndVerifyClaims(token: string, key: string): ClaimInterface {
+        let claim;
+        try {
+            claim = jwt.verify(token, key) as ClaimInterface;
+        } catch(err) {
+            throw new Error("Invalid jwt: " + err.message);
+        }
+
+        const currentSeconds = Math.floor( (new Date()).valueOf() / 1000);
+        if (currentSeconds > claim.exp || currentSeconds < claim.auth_time) {
+            throw new Error('Claim is expired or invalid');
+        }
+        if (claim.iss !== this.auth0Issuer) {
+            throw new Error('Claim issuer is invalid');
+        }
+
+        if (claim.token_use !== 'access') {
+            throw new Error('Claim use is not access');
+        }
+
+        return claim;
+    }
+
+    private getKeyFromToken(token: string, pems:{[key: string]: string}): string {
+        const header = this.getTokenHeader(token);
+        const key = pems[header.kid];
+        if (key === undefined) {
+            throw new Error('Claim made for unknown kid');
+        }
+        return key;
+    }
+
+    private getTokenHeader(token): TokenHeaderInterface {
+        const tokenSections = (token || '').split('.');
+        if (tokenSections.length < 2) {
+            throw new Error('Token is invalid');
+        }
+        const headerJSON = Buffer.from(tokenSections[0], 'base64').toString('utf8');
+        return  JSON.parse(headerJSON) as TokenHeaderInterface;
+    }
+}
