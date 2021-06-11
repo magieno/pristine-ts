4ba3b166109e0f6a182645f60e615713684f42a4
mathieugh <mathieu.girouxh@gmail.com>
2021-06-11 15:30:52 -0400

    - Add auth0 domain config

--- packages/auth0/src/auth0.module.ts
+++ packages/auth0/src/auth0.module.ts
@@ -1,6 +1,7 @@
 import {ModuleInterface} from "@pristine-ts/common";
 import {Auth0ModuleKeyname} from "./auth0.module.keyname";
 import {HttpModule} from "@pristine-ts/http";
+import {EnvironmentVariableResolver} from "@pristine-ts/configuration";
 
 
 export * from "./auth0.module.keyname";
@@ -8,6 +9,13 @@ export * from "./auth0.module.keyname";
 export const Auth0Module: ModuleInterface = {
     keyname: Auth0ModuleKeyname,
     configurationDefinitions: [
+        {
+            parameterName: Auth0ModuleKeyname + ".domain",
+            isRequired: true,
+            defaultResolvers: [
+                new EnvironmentVariableResolver("AUTH0_DOMAIN"),
+            ]
+        },
     ],
     importModules: [HttpModule],
 }
