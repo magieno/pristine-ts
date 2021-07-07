f0d5676a026ea5551185ce09bad96aac250f6c23
mathieugh <mathieu.girouxh@gmail.com>
2021-07-07 15:55:08 -0400

    - Chain resolves

--- packages/aws/src/resolvers/ssm.resolver.ts
+++ packages/aws/src/resolvers/ssm.resolver.ts
@@ -3,23 +3,39 @@ import {SSM, GetParameterRequest} from "@aws-sdk/client-ssm";
 import {SSMResolverError} from "../errors/ssm-resolver.error";
 
 export class SSMResolver implements ResolverInterface<string> {
-    public constructor(private readonly ssmParameterName: string, private readonly region: string, private readonly isSecure: boolean = false) { }
+    public constructor(private readonly ssmParameterName: string | ResolverInterface<string> , private readonly region: string | ResolverInterface<string> , private readonly isSecure: boolean = false) { }
 
     async resolve(): Promise<string> {
-        const ssm: SSM = new SSM({apiVersion: 'latest', region: this.region});
+        const ssmParameterName = await this.resolveArgument(this.ssmParameterName);
+        const region = await this.resolveArgument(this.region);
+        const ssm: SSM = new SSM({apiVersion: 'latest', region});
         const params: GetParameterRequest = {
-            Name: this.ssmParameterName,
+            Name: ssmParameterName,
             WithDecryption: this.isSecure,
         }
         try {
             const parameterOutput = await ssm.getParameter(params);
             if(!parameterOutput.Parameter?.Value) {
-                throw new SSMResolverError("No value for this parameter.", this.ssmParameterName);
+                throw new SSMResolverError("No value for this parameter.", ssmParameterName);
             }
             return Promise.resolve(parameterOutput.Parameter?.Value as string);
 
         } catch (e) {
-            throw new SSMResolverError("Error getting parameter from SSM.", this.ssmParameterName, e);
+            throw new SSMResolverError("Error getting parameter from SSM.", ssmParameterName, e);
         }
+    }
+
+    private async resolveArgument(value: string | ResolverInterface<string> ): Promise<string> {
+        if(typeof value === "string") {
+            return value;
+        }
+
+        if(typeof value === "object" && typeof value.resolve === "function") {
+            return this.resolveArgument(await value.resolve());
+        }
+
+        throw new SSMResolverError("Cannot resolve the value passed. It isn't of type string or ResolverInterface.", {
+            value,
+        });
     }
 }
