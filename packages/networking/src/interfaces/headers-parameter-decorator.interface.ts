baf3a3d14202fc166e5042dd4ead12a3070a05b6
mathieugh <mathieu.girouxh@gmail.com>
2021-07-13 17:11:11 -0400

    Fix comment

--- packages/networking/src/interfaces/headers-parameter-decorator.interface.ts
+++ packages/networking/src/interfaces/headers-parameter-decorator.interface.ts
@@ -2,7 +2,7 @@ import {ParameterDecoratorInterface} from "./parameter-decorator.interface";
 
 /**
  * This interface represents the object that will contain the information to know how to handle the
- * @request decorator.
+ * @headers decorator.
  */
 export interface HeadersParameterDecoratorInterface extends ParameterDecoratorInterface{
     type: "headers";
