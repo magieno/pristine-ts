a4a45a1832ab1c9d7c5d5f873d296f72bb227521
mathieugh <mathieu.girouxh@gmail.com>
2021-08-02 15:40:18 -0400

    - Added doc on dynamic table decorator

--- packages/aws/src/decorators/dynamic-table-name.decorator.ts
+++ packages/aws/src/decorators/dynamic-table-name.decorator.ts
@@ -2,6 +2,12 @@ import {DynamicTableNameModel} from "../models/dynamic-table-name.model";
 
 export const dynamicTableNameRegistry: DynamicTableNameModel[] = [];
 
+/**
+ * This decorator is to be put on a class that will be used with DynamoDb.
+ * This registry will be used to set the DynamoDbTable Symbol of the class to the value that the token resolves to.
+ * This will be done in the after init of the Aws module.
+ * @param name The name of the variable in the container containing the name of the table.
+ */
 export const dynamicTableName = (name: string) => {
     return (constructor: any) => {
         dynamicTableNameRegistry.push({
