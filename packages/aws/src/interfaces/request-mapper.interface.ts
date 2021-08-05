11b5831aeb324127adc01872ab1388ba47089ab9
mathieugh <mathieu.girouxh@gmail.com>
2021-08-05 10:16:28 -0400

    - Commented request mappers

--- packages/aws/src/interfaces/request-mapper.interface.ts
+++ packages/aws/src/interfaces/request-mapper.interface.ts
@@ -2,5 +2,9 @@ import {RequestInterface} from "@pristine-ts/common";
 import {ApiGatewayRequest} from "../types/api-gateway-request.type";
 
 export interface RequestMapperInterface {
+    /**
+     * Maps an Api gateway request to a Pristine request.
+     * @param request The api gateway request.
+     */
     map(request: ApiGatewayRequest): RequestInterface;
 }
