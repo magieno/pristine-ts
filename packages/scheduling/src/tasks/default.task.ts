93415b1e1fbb41b8ead9b56a70d0fab00cf56be3
Etienne Noël <etienne.noel@appdirect.com>
2021-09-06 09:12:45 -0700

    Fixed one tiny issue.

--- packages/scheduling/src/tasks/default.task.ts
+++ packages/scheduling/src/tasks/default.task.ts
@@ -1,7 +1,10 @@
 import {ScheduledTaskInterface} from "../interfaces/scheduled-task.interface";
+import {injectable} from "tsyringe";
+import {ServiceDefinitionTagEnum, tag} from "@pristine-ts/common";
 
+@injectable()
+@tag(ServiceDefinitionTagEnum.ScheduledTask)
 export class DefaultTask implements ScheduledTaskInterface{
-    run() {
+    async run(): Promise<void> {
     }
-
 }
