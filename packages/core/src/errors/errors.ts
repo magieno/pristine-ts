5fecf82de9aebf636ba139780d64f9c008af21d6
mathieugh <mathieu.girouxh@gmail.com>
2021-09-07 14:07:39 -0400

    - Fix exports in core

--- packages/core/src/errors/errors.ts
+++ packages/core/src/errors/errors.ts
@@ -1,5 +1,7 @@
 export * from "./error-response-interception-execution.error";
 export * from "./provider-registration.error";
+export * from "./event-interception-execution.error";
+export * from "./kernel-initialization.error";
 export * from "./request-handling.error";
 export * from "./request-interception-execution.error";
 export * from "./response-interception-execution.error";
--- packages/core/src/interfaces/interfaces.ts
+++ packages/core/src/interfaces/interfaces.ts
@@ -1,3 +1,4 @@
 export * from "./error-response-interceptor.interface";
+export * from "./event-interceptor.interface";
 export * from "./request-interceptor.interface";
 export * from "./response-interceptor.interface";
