e692bee74a2ac2878633b83ee698c17ef18a406c
mathieugh <mathieu.girouxh@gmail.com>
2021-09-08 15:53:10 -0400

    - Added doc for event module

--- packages/event/src/errors/event-transform.error.ts
+++ packages/event/src/errors/event-transform.error.ts
@@ -1,8 +1,17 @@
 import {EventParserInterface} from "../interfaces/event-parser.interface";
 import {LoggableError} from "@pristine-ts/common";
 
+/**
+ * This Error is thrown when an error happens while trying to transform an event.
+ */
 export class EventTransformError extends LoggableError {
 
+    /**
+     * This Error is thrown when an error happens while trying to transform an event.
+     * @param message The error message.
+     * @param event The event that caused the error.
+     * @param eventParsers The event parsers that were registered.
+     */
     public constructor(message: string, event: any, eventParsers: EventParserInterface<any>[]) {
         super(message, {
             event,
--- packages/event/src/models/event.ts
+++ packages/event/src/models/event.ts
@@ -1,3 +1,7 @@
+/**
+ * This model defines what an Event is in the Pristine library.
+ * Once an event parsed, this should be the only object that will be handle inside the library.
+ */
 export class Event<T> {
     type: string;
     payload: T;
