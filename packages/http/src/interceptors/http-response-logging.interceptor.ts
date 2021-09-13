c47c4560722f1c1ab34086f69d321793dd422319
mathieugh <mathieu.girouxh@gmail.com>
2021-09-13 13:40:21 -0400

    - Documented http interceptors

--- packages/http/src/interceptors/http-response-logging.interceptor.ts
+++ packages/http/src/interceptors/http-response-logging.interceptor.ts
@@ -6,12 +6,22 @@ import {LogHandlerInterface} from "@pristine-ts/logging";
 import {HttpResponseInterceptorInterface} from "../interfaces/http-response-interceptor.interface";
 import {HttpResponseInterface} from "../interfaces/http-response.interface";
 
+/**
+ * This class is an interceptor to log incoming http responses.
+ * It is tagged as an HttpResponseInterceptor so it can be automatically injected with the all the other HttpResponseInterceptors.
+ */
 @tag(ServiceDefinitionTagEnum.HttpResponseInterceptor)
 @injectable()
 export class HttpResponseLoggingInterceptor implements HttpResponseInterceptorInterface {
     constructor(@inject("LogHandlerInterface") private readonly logHandler: LogHandlerInterface) {
     }
 
+    /**
+     * This method intercepts an incoming http response and logs it.
+     * @param request
+     * @param options
+     * @param response
+     */
     async interceptResponse(request: HttpRequestInterface, options: HttpRequestOptions, response: HttpResponseInterface): Promise<HttpResponseInterface> {
         this.logHandler.info("Receiving http response", {response, options});
         return response;
