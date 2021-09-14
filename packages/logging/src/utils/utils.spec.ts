8c8dea8397651b22d490bacc5c1fccfb06c1a44c
Etienne Noel <etienne.noel@ima-tech.ca>
2021-09-14 11:30:07 -0400

    Fix the logging.

--- packages/logging/src/utils/utils.spec.ts
+++ packages/logging/src/utils/utils.spec.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata"
 import {Utils} from "./utils";
+import {LogModel} from "../models/log.model";
+import {SeverityEnum} from "../enums/severity.enum";
+import {OutputModeEnum} from "../enums/output-mode.enum";
 
 describe("Utils", () => {
 
@@ -19,6 +22,51 @@ describe("Utils", () => {
         expect(Utils.truncate(object, 3)).toEqual({bonjour:{allo:{bye:"byebye", date: new Date(1615830493000)}}});
         expect(Utils.truncate(object, 4)).toEqual({bonjour:{allo:{bye:"byebye", date: new Date(1615830493000)}}});
     });
+
+    it("should properly output a log", async() => {
+        const logInfo = new LogModel();
+        logInfo.date = new Date("2021-01-01");
+        logInfo.message = "Log info";
+        logInfo.severity = SeverityEnum.Info;
+        logInfo.extra = {
+            potato: "extra 1"
+        };
+
+        const logDebug = new LogModel();
+        logDebug.date = new Date("2021-01-01");
+        logDebug.message = "Log debug";
+        logDebug.severity = SeverityEnum.Debug;
+        logDebug.extra = {
+            potato: "extra 1"
+        };
+
+        const logWarning = new LogModel();
+        logWarning.date = new Date("2021-01-01");
+        logWarning.message = "Log warning";
+        logWarning.severity = SeverityEnum.Warning;
+        logWarning.extra = {
+            potato: "extra 1"
+        };
+
+        const logError = new LogModel();
+        logError.date = new Date("2021-01-01");
+        logError.message = "Log error";
+        logError.severity = SeverityEnum.Error;
+        logError.extra = {
+            potato: "extra 1"
+        };
+
+        const logCritical = new LogModel();
+        logCritical.date = new Date("2021-01-01");
+        logCritical.message = "Log critical";
+        logCritical.severity = SeverityEnum.Critical;
+        logCritical.extra = {
+            potato: "extra 1",
+        };
+
+        expect(Utils.outputLog(logCritical, OutputModeEnum.Json, 10))
+            .toEqual("{\"severity\":\"CRITICAL\",\"message\":\"Log critical\",\"date\":\"2021-01-01T00:00:00.000Z\",\"extra\":{\"potato\":\"extra 1\"}}")
+    })
 
     it("should consider string as a flat type", async () => {
 
