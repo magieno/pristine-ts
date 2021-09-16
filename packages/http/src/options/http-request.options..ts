f2074579b9616c2155c6f88d9d08bfd5e3bcc2fc
mathieugh <mathieu.girouxh@gmail.com>
2021-09-16 14:33:57 -0400

    Doc for options

--- packages/http/src/options/http-request.options..ts
+++ packages/http/src/options/http-request.options..ts
@@ -2,10 +2,32 @@ import {ResponseTypeEnum} from "../enums/response-type.enum";
 import {HttpRequestInterface} from "../interfaces/http-request.interface";
 import {HttpResponseInterface} from "../interfaces/http-response.interface";
 
+/**
+ * This interface defines what is an Http request options.
+ */
 export interface HttpRequestOptions {
+    /**
+     * The response type that is expected. Either raw or parsed as a JSON.
+     */
     responseType?: ResponseTypeEnum;
+
+    /**
+     * Whether or not to follow redirects when receiving a response that has a redirect status code.
+     */
     followRedirects?: boolean;
+
+    /**
+     * The maximum number of redirects to follow.
+     */
     maximumNumberOfRedirects?: number;
+
+    /**
+     * A function that evaluates whether or not the request should be retried when getting an error in the response.
+     */
     isRetryable?: ((httpRequestInterface: HttpRequestInterface, httpResponseInterface: HttpResponseInterface) => boolean);
+
+    /**
+     * The maximum number of time a request can be retried.
+     */
     maximumNumberOfRetries?: number;
 }
