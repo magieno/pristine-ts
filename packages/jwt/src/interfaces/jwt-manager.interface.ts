fbc2b917c258c98cfcb3dbb5e229bdae8e4fdc8d
mathieugh <mathieu.girouxh@gmail.com>
2021-09-17 13:41:02 -0400

    Doc for jwt interfaces

--- packages/jwt/src/interfaces/jwt-manager.interface.ts
+++ packages/jwt/src/interfaces/jwt-manager.interface.ts
@@ -1,5 +1,12 @@
 import {RequestInterface} from "@pristine-ts/common";
 
+/**
+ * The JwtManager Interface defines the methods that a JWT manager must implement.
+ */
 export interface JwtManagerInterface {
+    /**
+     * Validates and returns the decoded JWT.
+     * @param request The request containing the JWT.
+     */
     validateAndDecode(request: RequestInterface): Promise<any>;
 }
--- packages/jwt/src/interfaces/jwt-payload-decorator.interface.ts
+++ packages/jwt/src/interfaces/jwt-payload-decorator.interface.ts
@@ -1,5 +1,8 @@
 import {ParameterDecoratorInterface} from "@pristine-ts/networking";
 
+/**
+ * The JwtPayloadDecoratorInterface defines what a JWT payload decorator needs.
+ */
 export interface JwtPayloadDecoratorInterface extends ParameterDecoratorInterface{
     type: "jwtPayload"
 }
