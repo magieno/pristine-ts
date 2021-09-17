5ce1e9e173f9e17b8d88fc7e1aab244739ba7230
mathieugh <mathieu.girouxh@gmail.com>
2021-09-17 13:40:46 -0400

    Doc for jwt errors

--- packages/jwt/src/errors/invalid-jwt.error.ts
+++ packages/jwt/src/errors/invalid-jwt.error.ts
@@ -1,10 +1,19 @@
+import {LoggableError, RequestInterface} from "@pristine-ts/common";
+
 /**
  * This Error is thrown when you try to decode a JWT but the token is invalid.
  */
-import {LoggableError, RequestInterface} from "@pristine-ts/common";
-
 export class InvalidJwtError extends LoggableError {
 
+    /**
+     * This Error is thrown when you try to decode a JWT but the token is invalid.
+     * @param message The error message to show.
+     * @param previousError The previous error.
+     * @param request The request that contained the JWT.
+     * @param token The actual JWT.
+     * @param algorithm The algorithm used to decode the JWT.
+     * @param publicKey The public key used to decode the JWT.
+     */
     public constructor(message: string, previousError: Error, request: RequestInterface, token, algorithm, publicKey) {
         super(message, {
             request,
--- packages/jwt/src/errors/jwt-authorization-header.error.ts
+++ packages/jwt/src/errors/jwt-authorization-header.error.ts
@@ -1,9 +1,14 @@
+import {LoggableError, RequestInterface} from "@pristine-ts/common";
+
 /**
  * This Error is thrown when there's you try to decode a JWT in a request but the AuthorizationHeader is missing.
  */
-import {LoggableError, RequestInterface} from "@pristine-ts/common";
-
 export class JwtAuthorizationHeaderError extends LoggableError {
+    /**
+     * This Error is thrown when there's you try to decode a JWT in a request but the AuthorizationHeader is missing.
+     * @param message The error message.
+     * @param request The request that is missing the AuthorizationHeader.
+     */
     public constructor(message: string, request: RequestInterface) {
         super(message, {
             request,
