84560ff3d998b5492507e9608d655a2a1f326726
mathieugh <mathieu.girouxh@gmail.com>
2021-09-21 12:29:51 -0400

    - Doc for jwt payload decorator

--- packages/jwt/src/decorators/jwt-payload.decorators.ts
+++ packages/jwt/src/decorators/jwt-payload.decorators.ts
@@ -1,11 +1,27 @@
 import {JwtPayloadDecoratorInterface} from "../interfaces/jwt-payload-decorator.interface";
 
+/**
+ * This decorator can be used to inject the decoded JWT payload of a request in a parameter of a method in a controller.
+ */
 export const jwtPayload = () => {
     return (
+        /**
+         * The class on which the decorator is used.
+         */
         target: Object,
+
+        /**
+         * The method on which the decorator is used.
+         */
         propertyKey: string | symbol,
+
+        /**
+         * The index of the parameter for which the decorator is used.
+         */
         parameterIndex: number
     ) => {
+        // Verify that the object target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"] exists or we create it.
+        // This object is a convention defined by Pristine on where to save controller method parameter decorator information and is used in the router to retrieve that information.
         if (target.constructor.prototype.hasOwnProperty("__metadata__") === false) {
             target.constructor.prototype["__metadata__"] = {}
         }
@@ -22,10 +38,12 @@ export const jwtPayload = () => {
             target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"] = [];
         }
 
+        // Set the type of method parameter. Each decorator has it's own type.
         const methodParameter: JwtPayloadDecoratorInterface = {
             type: "jwtPayload"
         };
 
+        // Save the method parameter with the proper parameter index (index of the parameter in the list of parameters of a method).
         target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"][parameterIndex] = methodParameter;
     }
 };
