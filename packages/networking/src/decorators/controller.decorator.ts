fd085a095fb34127a7d34e62c483069e1f8b19d2
mathieugh <mathieu.girouxh@gmail.com>
2021-09-28 15:36:15 -0400

    - Doc for controller decorator

--- packages/networking/src/decorators/controller.decorator.ts
+++ packages/networking/src/decorators/controller.decorator.ts
@@ -1,7 +1,18 @@
 export const controllerRegistry: any[] = [];
 
+/**
+ * The controller decorator can be used on a class to register this class as a controller in the router.
+ * @param basePath The base path for all the routes in the controller.
+ */
 export const controller = (basePath: string) => {
-    return (constructor: Function) => {
+    return (
+        /**
+         * The constructor of the class
+         */
+        constructor: Function
+    ) => {
+        // Verify that the object constructor.prototype["__metadata__"]["controller"][basePath] exists or we create it.
+        // This object is a convention defined by Pristine on where to save controller decorator information and is used in the router to retrieve that information.
         if(constructor.prototype.hasOwnProperty("__metadata__") === false) {
             constructor.prototype["__metadata__"] = {}
         }
@@ -11,8 +22,10 @@ export const controller = (basePath: string) => {
             constructor.prototype["__metadata__"]["controller"] = {}
         }
 
+        // Save the base path.
         constructor.prototype["__metadata__"]["controller"]["basePath"] = basePath;
 
+        // Push the class prototype in the controllerRegistry that is used to instantiate all the controllers for the router.
         controllerRegistry.push(constructor.prototype)
     }
 }
