d5fbc51f1ce9ed0ab36273675a4c919abd8d47dc
mathieugh <mathieu.girouxh@gmail.com>
2021-10-01 08:41:45 -0400

    - Doc for query-parameters decorator

--- packages/networking/src/decorators/query-parameters.decorator.ts
+++ packages/networking/src/decorators/query-parameters.decorator.ts
@@ -1,11 +1,27 @@
 import {QueryParametersDecoratorInterface} from "../interfaces/query-parameters-decorator.interface";
 
+/**
+ * The queryParameters decorator can be used to inject all the query parameters of a request in a parameter of a method in a controller.
+ */
 export const queryParameters = () => {
     return (
+        /**
+         * The class on which the decorator is used.
+         */
         target: Object,
+
+        /**
+         * The method on which the decorator is used.
+         */
         propertyKey: string | symbol,
+
+        /**
+         * The index of the parameter for which the decorator is used.
+         */
         parameterIndex: number
     ) => {
+        // Verify that the object target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"] exists or we create it.
+        // This object is a convention defined by Pristine on where to save controller method parameter decorator information and is used in the router to retrieve that information.
         if(target.constructor.prototype.hasOwnProperty("__metadata__") === false) {
             target.constructor.prototype["__metadata__"] = {}
         }
@@ -22,10 +38,12 @@ export const queryParameters = () => {
             target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"] = [];
         }
 
+        // Set the type of method parameter. Each parameter decorator has it's own type.
         const methodParameter: QueryParametersDecoratorInterface = {
             type: "queryParameters",
         };
 
+        // Save the method parameter with the proper parameter index (index of the parameter in the list of parameters of a method).
         target.constructor.prototype["__metadata__"]["methods"][propertyKey]["arguments"][parameterIndex] = methodParameter;
     }
 }
