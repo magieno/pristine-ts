cabed038d02ceabe75069cdcf6e4d0ce7c219195
mathieugh <mathieu.girouxh@gmail.com>
2021-10-27 15:11:39 -0400

    - Doc for errors

--- packages/networking/src/errors/path-router-instantiation.error.ts
+++ packages/networking/src/errors/path-router-instantiation.error.ts
@@ -1,9 +1,10 @@
-/**
- * This Error is thrown when there's an error that happens when the networking is being initialized
- */
 import {LoggableError} from "@pristine-ts/common";
 import {PathRouterNode} from "../nodes/path-router.node";
 
+/**
+ * This Error is thrown when there's an error that happens when the networking is being initialized.
+ * It is thrown when an error occurs when trying to instantiate a PathRouterNode.
+ */
 export class PathRouterInstantiationError extends LoggableError {
 
     public constructor(message: string, path: string, parent?: PathRouterNode) {
