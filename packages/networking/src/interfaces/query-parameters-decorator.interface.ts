004960c5c6778d8f2bf8ef4c01ddc84805c3fe00
mathieugh <mathieu.girouxh@gmail.com>
2021-11-01 15:43:08 -0400

    - Doc for interfaces

--- packages/networking/src/interfaces/query-parameters-decorator.interface.ts
+++ packages/networking/src/interfaces/query-parameters-decorator.interface.ts
@@ -1,5 +1,9 @@
 import {ParameterDecoratorInterface} from "./parameter-decorator.interface";
 
+/**
+ * This interface represents the object that will contain the information required to know how to handle the
+ * @queryParameters decorator.
+ */
 export interface QueryParametersDecoratorInterface extends ParameterDecoratorInterface {
     type: "queryParameters";
 }
