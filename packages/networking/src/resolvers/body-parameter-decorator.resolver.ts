b9ac5562c9306a536190667c8248a93334dd4929
mathieugh <mathieu.girouxh@gmail.com>
2021-11-16 13:20:32 -0500

    - Started docs for resolvers

--- packages/networking/src/resolvers/body-parameter-decorator.resolver.ts
+++ packages/networking/src/resolvers/body-parameter-decorator.resolver.ts
@@ -7,10 +7,24 @@ import {NetworkingModuleKeyname} from "../networking.module.keyname";
 import {ParameterDecoratorInterface} from "../interfaces/parameter-decorator.interface";
 import {BodyParameterDecoratorInterface} from "../interfaces/body-parameter-decorator.interface";
 
+/**
+ * The BodyParameterDecoratorResolver resolves the value of the body of the request so that it can be injected it into the
+ * parameter of the route of the controller that was annotated with the @body decorator.
+ * It is tagged as an MethodParameterDecoratorResolver so it can be automatically injected with the all the other MethodParameterDecoratorResolvers.
+ */
 @moduleScoped(NetworkingModuleKeyname)
 @tag(ServiceDefinitionTagEnum.MethodParameterDecoratorResolver)
 @injectable()
 export class BodyParameterDecoratorResolver implements ControllerMethodParameterDecoratorResolverInterface {
+
+    /**
+     * Resolves the value of the body of the request.
+     * The router than injects that value into the parameter of the controller method.
+     * @param methodArgument The method argument created by the decorator.
+     * @param request The request
+     * @param routeParameters The router parameters
+     * @param identity The identity making the request
+     */
     resolve(methodArgument: BodyParameterDecoratorInterface,
             request: Request,
             routeParameters: { [p: string]: string },
@@ -18,6 +32,11 @@ export class BodyParameterDecoratorResolver implements ControllerMethodParameter
         return Promise.resolve(request.body ?? null);
     }
 
+    /**
+     * Returns whether or not the resolver support such a method argument.
+     * Usually we will check the methodArgument.type field to determine if it is a supported type.
+     * @param methodArgument
+     */
     supports(methodArgument: ParameterDecoratorInterface): boolean {
         return methodArgument && methodArgument.hasOwnProperty("type") && methodArgument.type === "body";
     }
