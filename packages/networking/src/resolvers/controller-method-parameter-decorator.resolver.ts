41f24a657f653f2172729c4e6cb79828cef8bc77
mathieugh <mathieu.girouxh@gmail.com>
2021-12-07 15:55:43 -0500

    - Commented the controller method parameter decorator resolver

--- packages/networking/src/resolvers/controller-method-parameter-decorator.resolver.ts
+++ packages/networking/src/resolvers/controller-method-parameter-decorator.resolver.ts
@@ -4,11 +4,26 @@ import {ControllerMethodParameterDecoratorResolverInterface} from "../interfaces
 import {Request} from "../models/request";
 import {ParameterDecoratorInterface} from "../interfaces/parameter-decorator.interface";
 
+/**
+ * This service calls the resolvers to resolve the value to be injected in the parameter of a method.
+ */
 @injectable()
 export class ControllerMethodParameterDecoratorResolver {
+    /**
+     * This service calls the resolvers to resolve the value to be injected in the parameter of a method.
+     * @param methodParameterDecoratorResolvers The parameter decorator resolvers. All services with the tag ServiceDefinitionTagEnum.MethodParameterDecoratorResolver will be automatically injected here.
+     */
     constructor(@injectAll(ServiceDefinitionTagEnum.MethodParameterDecoratorResolver) private readonly methodParameterDecoratorResolvers: ControllerMethodParameterDecoratorResolverInterface[]) {
     }
 
+
+    /**
+     * This method calls the parameter decorator resolver that supports the type of the decorator and resolves the value to be injected in the parameter.
+     * @param methodArgument The method argument created by the decorator.
+     * @param request The request
+     * @param routeParameters The router parameters
+     * @param identity The identity making the request
+     */
     public resolve(methodArgument: ParameterDecoratorInterface,
                    request: Request,
                    routeParameters: { [key: string]: string },
