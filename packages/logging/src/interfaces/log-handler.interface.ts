9b60e71641aa88d42700a7dc8f1bb4b0e7d545b5
mathieugh <mathieu.girouxh@gmail.com>
2021-12-20 16:44:53 -0500

    - Doc log handler interface

--- packages/logging/src/interfaces/log-handler.interface.ts
+++ packages/logging/src/interfaces/log-handler.interface.ts
@@ -1,12 +1,45 @@
-
+/**
+ * This interface defines what functions should be implemented by a LogHandler.
+ * This interface is what should be injected when you want a LogHandler so that mocking can be done easier.
+ */
 export interface LogHandlerInterface {
-    error(message: string, extra?: any, module?: string): void;
-
+    /**
+     * Logs the message if the severity is set to critical or above.
+     * @param message The message to log.
+     * @param extra The extra object to log.
+     * @param module The module from where the log was created.
+     */
     critical(message: string, extra?: any, module?: string): void;
 
-    debug(message: string, extra?: any, module?: string): void;
+    /**
+     * Logs the message if the severity is set to critical or above.
+     * @param message The message to log.
+     * @param extra The extra object to log.
+     * @param module The module from where the log was created.
+     */
+    error(message: string, extra?: any, module?: string): void;
 
+    /**
+     * Logs the message if the severity is set to critical or above.
+     * @param message The message to log.
+     * @param extra The extra object to log.
+     * @param module The module from where the log was created.
+     */
+    warning(message: string, extra?: any, module?: string): void;
+
+    /**
+     * Logs the message if the severity is set to critical or above.
+     * @param message The message to log.
+     * @param extra The extra object to log.
+     * @param module The module from where the log was created.
+     */
     info(message: string, extra?: any, module?: string): void;
 
-    warning(message: string, extra?: any, module?: string): void;
+    /**
+     * Logs the message if the severity is set to critical or above.
+     * @param message The message to log.
+     * @param extra The extra object to log.
+     * @param module The module from where the log was created.
+     */
+    debug(message: string, extra?: any, module?: string): void;
 }
