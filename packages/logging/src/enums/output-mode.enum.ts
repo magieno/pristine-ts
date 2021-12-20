7213364ecd77e10bf935b21d50839abb99e6c406
mathieugh <mathieu.girouxh@gmail.com>
2021-12-20 12:04:55 -0500

    - Doc for output modes

--- packages/logging/src/enums/output-mode.enum.ts
+++ packages/logging/src/enums/output-mode.enum.ts
@@ -1,4 +1,13 @@
+/**
+ * Output mode for the logs.
+ */
 export enum OutputModeEnum {
+    /**
+     * The logs will output a JSON format.
+     */
     Json = "JSON",
+    /**
+     * The logs will output a simple string format.
+     */
     Simple = "SIMPLE",
 }
