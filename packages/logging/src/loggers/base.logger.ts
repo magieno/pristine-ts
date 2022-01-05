dad2d0cb89339829712633b2aeb86de48a5d4ef6
mathieugh <mathieu.girouxh@gmail.com>
2022-01-05 15:49:45 -0500

    - Doc for base logger

--- packages/logging/src/loggers/base.logger.ts
+++ packages/logging/src/loggers/base.logger.ts
@@ -2,12 +2,40 @@ import {LogModel} from "../models/log.model";
 import {SeverityEnum} from "../enums/severity.enum";
 import {Utils} from "../utils/utils";
 import {CommonModuleKeyname} from "@pristine-ts/common";
-import {Readable} from "stream";
 import {OutputModeEnum} from "../enums/output-mode.enum";
 
+/**
+ * The BaseLogger is the base abstract class that all loggers should extend.
+ * It defines the basic logic that applies to all loggers.
+ */
 export abstract class BaseLogger {
+
+    /**
+     * The stacked logs are the logs that were not outputted right away but that will need to be outputted if a log with a higher severity arrives.
+     * @private
+     */
     private stackedLogs: { [key: string]: LogModel[] } = {};
 
+    /**
+     * The BaseLogger is the base abstract class that all loggers should extend.
+     * It defines the basic logic that applies to all loggers.
+     * @param numberOfStackedLogs The number of logs to keep in the stack and to print once a log with a high enough severity arrives.
+     * @param logSeverityLevelConfiguration The number representing the severity from which logs should be outputted.
+     * For example, if this is set to 3, any log that has a severity of Error(3) or critical(4) will be outputted.
+     * @param logDebugDepthConfiguration The number of level to go down in an object when printing a log with the Debug severity.
+     * We often do not need to go to the bottom layer of an object, so we can truncate at a certain depth.
+     * @param logInfoDepthConfiguration The number of level to go down in an object when printing a log with the Info severity.
+     * We often do not need to go to the bottom layer of an object, so we can truncate at a certain depth.
+     * @param logWarningDepthConfiguration The number of level to go down in an object when printing a log with the Warning severity.
+     * We often do not need to go to the bottom layer of an object, so we can truncate at a certain depth.
+     * @param logErrorDepthConfiguration The number of level to go down in an object when printing a log with the Error severity.
+     * We often do not need to go to the bottom layer of an object, so we can truncate at a certain depth.
+     * @param logCriticalDepthConfiguration The number of level to go down in an object when printing a log with the Critical severity.
+     * We often do not need to go to the bottom layer of an object, so we can truncate at a certain depth.
+     * @param isActivated Whether or not this particular logger is activated and should output logs.
+     * @param outputMode The output mode, that the logger should use.
+     * @param spaces The number of spaces to indent the outputted logs.
+     */
     constructor(
         protected readonly numberOfStackedLogs: number,
         protected readonly logSeverityLevelConfiguration: number,
@@ -21,13 +49,24 @@ export abstract class BaseLogger {
         protected readonly spaces = 2) {
     }
 
+    /**
+     * Initializes the logger. To be implemented in each logger.
+     * @protected
+     */
     protected abstract initialize();
 
+    /**
+     * Returns whether this particular logger is active and should output logs.
+     */
     public isActive(): boolean {
         return this.isActivated;
     }
 
-    public outputLog(log: LogModel): string {
+    /**
+     * Gets the formatted output log based on a log model.
+     * @param log The log to be formatted.
+     */
+    public getFormattedOutputLog(log: LogModel): string {
         switch (log.severity) {
             case SeverityEnum.Debug:
                 return Utils.outputLog(log, this.outputMode, this.logDebugDepthConfiguration);
@@ -46,8 +85,18 @@ export abstract class BaseLogger {
         }
     }
 
+    /**
+     * Actually outputs the log. To be implemented in each logger.
+     * @param log The log to be outputted.
+     * @protected
+     */
     protected abstract log(log: LogModel): void;
 
+    /**
+     * Captures the log and evaluates which logs need to be outputted or stacked.
+     * @param log The log to be captured.
+     * @protected
+     */
     protected captureLog(log: LogModel): void {
         if (this.numberOfStackedLogs > 0) {
             this.setupStackedLogsArrayIfRequired(log.traceId);
@@ -67,6 +116,11 @@ export abstract class BaseLogger {
         }
     }
 
+    /**
+     * Sets up the stack of logs if it is required.
+     * @param traceId Optional trace id to stack logs based on different requests.
+     * @private
+     */
     private setupStackedLogsArrayIfRequired(traceId?: string) {
         if (this.stackedLogs.hasOwnProperty(CommonModuleKeyname) === false) {
             this.stackedLogs[CommonModuleKeyname] = [];
@@ -77,6 +131,11 @@ export abstract class BaseLogger {
         }
     }
 
+    /**
+     * Adds the log to the stacked logs and makes sure we only keep the right amount of stacked logs.
+     * @param log The log to be added.
+     * @private
+     */
     private addStackedLog(log: LogModel) {
         const stackedLogsKey = log.traceId ?? CommonModuleKeyname;
 
@@ -91,6 +150,11 @@ export abstract class BaseLogger {
         }
     }
 
+    /**
+     * Outputs the stacked logs.
+     * @param traceId Optional trace id so that we only output the stacked logs for this trace.
+     * @private
+     */
     private outputStackedLogs(traceId?: string) {
         for (const log of this.stackedLogs[CommonModuleKeyname]) {
             this.log(log);
@@ -107,6 +171,11 @@ export abstract class BaseLogger {
         this.clearStackedLogs(traceId);
     }
 
+    /**
+     * Clears the stacked logs.
+     * @param traceId Optional trace id so that we only remove the stacked logs for this trace.
+     * @private
+     */
     private clearStackedLogs(traceId?: string) {
         this.stackedLogs[CommonModuleKeyname] = [];
 
