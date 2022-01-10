6a45e8cb5c43fe5f6b16f25063db22f70ce5b1f6
mathieugh <mathieu.girouxh@gmail.com>
2022-01-10 18:06:27 -0500

    - Doc for diagnostic model

--- packages/logging/src/models/diagnostics.model.ts
+++ packages/logging/src/models/diagnostics.model.ts
@@ -1,15 +1,60 @@
+/**
+ * The diagnostic model represents detailed information to help debug that will be outputted in the extras of the log
+ * when diagnostic is enabled.
+ */
 export class DiagnosticsModel {
+    /**
+     * The node version on which the code was executed.
+     */
     nodeVersion: string;
+
+    /**
+     * An array representing the stack trace from which the log originated.
+     */
     stackTrace: {
+        /**
+         * The class name at which it was called in the stack trace.
+         */
         className: string;
+
+        /**
+         * The class name at which it was called in the stack trace.
+         */
         filename: string;
+
+        /**
+         * The line at which it was called in the stack trace
+         */
         line: string;
+
+        /**
+         * The column at which it was called in the stack trace
+         */
         column: string;
     }[] = [];
+
+    /**
+     * The very last element of the stack trace to know precisely where the log originated from.
+     */
     lastStackTrace: {
+        /**
+         * The class name at which it was called in the stack trace.
+         */
         className: string;
+
+        /**
+         * The class name at which it was called in the stack trace.
+         */
         filename: string;
+
+        /**
+         * The line at which it was called in the stack trace
+         */
         line: string;
+
+        /**
+         * The column at which it was called in the stack trace
+         */
         column: string;
     };
 }
