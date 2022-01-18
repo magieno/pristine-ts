7b8cb35a85e38b54a3c317f183822a87660ab206
mathieugh <mathieu.girouxh@gmail.com>
2022-01-18 11:18:01 -0500

    - Docs for scheduled task interface

--- packages/scheduling/src/interfaces/scheduled-task.interface.ts
+++ packages/scheduling/src/interfaces/scheduled-task.interface.ts
@@ -1,3 +1,9 @@
+/**
+ * This interface defines what a Scheduled task should implement.
+ */
 export interface ScheduledTaskInterface {
+    /**
+     * This method runs the actual task.
+     */
     run(): Promise<void>;
 }
