3b2d7f9a671daa4aa804e97e557c70119fc5a670
mathieugh <mathieu.girouxh@gmail.com>
2022-01-18 11:18:31 -0500

    - Docs for default task

--- packages/scheduling/src/tasks/default.task.ts
+++ packages/scheduling/src/tasks/default.task.ts
@@ -2,6 +2,11 @@ import {ScheduledTaskInterface} from "../interfaces/scheduled-task.interface";
 import {injectable} from "tsyringe";
 import {ServiceDefinitionTagEnum, tag} from "@pristine-ts/common";
 
+/**
+ * The default task is here so that there is at least one service with the ServiceDefinitionTagEnum.ScheduledTask.
+ * It does not do anything.
+ * Until there's a fix for: https://github.com/microsoft/tsyringe/issues/63
+ */
 @injectable()
 @tag(ServiceDefinitionTagEnum.ScheduledTask)
 export class DefaultTask implements ScheduledTaskInterface{
