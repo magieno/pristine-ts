d415d53786c1b5511b3f44e6276f9474a1bb60cc
mathieugh <mathieu.girouxh@gmail.com>
2022-01-18 11:18:23 -0500

    - Docs for manager

--- packages/scheduling/src/managers/scheduler.manager.ts
+++ packages/scheduling/src/managers/scheduler.manager.ts
@@ -5,10 +5,21 @@ import {SchedulingModuleKeyname} from "../scheduling.module.keyname";
 import {SchedulerInterface} from "../interfaces/scheduler.interface";
 import {LogHandlerInterface} from "@pristine-ts/logging";
 
+/**
+ * The Scheduler Manager runs the scheduled tasks.
+ * It is tagged so it can be injected using SchedulerInterface.
+ * It is module scoped to the scheduling module.
+ */
 @moduleScoped(SchedulingModuleKeyname)
 @tag("SchedulerInterface")
 @injectable()
 export class SchedulerManager implements SchedulerInterface {
+
+    /**
+     * The Scheduler Manager runs the scheduled tasks.
+     * @param scheduledTasks The scheduled tasks to run. All services with the tag ServiceDefinitionTagEnum.ScheduledTask will be automatically injected here.
+     * @param logHandler The log handler to use to output logs.
+     */
     constructor(@injectAll(ServiceDefinitionTagEnum.ScheduledTask) private readonly scheduledTasks: ScheduledTaskInterface[],
                 @inject("LogHandlerInterface") private readonly logHandler: LogHandlerInterface) {
     }
@@ -26,7 +37,7 @@ export class SchedulerManager implements SchedulerInterface {
                 promises.push(scheduledTask.run());
             });
 
-            this.logHandler.debug("Completed the execution of the tasks.", {scheduledTasks: this.scheduledTasks}, SchedulingModuleKeyname);
+            this.logHandler.debug("Completed triggering all the tasks.", {scheduledTasks: this.scheduledTasks}, SchedulingModuleKeyname);
 
             Promise.allSettled(promises).then(results => {
                 results.forEach(result => {
