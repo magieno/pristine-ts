14edea3448534c23c14d66450a64b526065b304e
mathieugh <mathieu.girouxh@gmail.com>
2022-01-19 09:08:55 -0500

    - Update manager

--- packages/scheduling/src/managers/scheduler.manager.ts
+++ packages/scheduling/src/managers/scheduler.manager.ts
@@ -6,7 +6,11 @@ import {SchedulerInterface} from "../interfaces/scheduler.interface";
 import {LogHandlerInterface} from "@pristine-ts/logging";
 
 /**
- * The Scheduler Manager runs the scheduled tasks.
+ * The Scheduler Manager runs the scheduled tasks.A scheduled task cannot specify the interval at which it wants to be triggered.
+ * It's only a mechanism to understand that must be triggered by a CRON.
+ * When used with a CRON, a scheduled task will be triggered at a recurring interval, it is your own responsibility to implement a
+ * logic of executing or not based on how long it's been since the last execution.
+ * Please look at the aws-scheduling module for an example of how to setup a CRON using AWS Event Bridge with Pristine's listeners.
  * It is tagged so it can be injected using SchedulerInterface.
  * It is module scoped to the scheduling module.
  */
@@ -16,7 +20,11 @@ import {LogHandlerInterface} from "@pristine-ts/logging";
 export class SchedulerManager implements SchedulerInterface {
 
     /**
-     * The Scheduler Manager runs the scheduled tasks.
+     * The Scheduler Manager runs the scheduled tasks.A scheduled task cannot specify the interval at which it wants to be triggered.
+     * It's only a mechanism to understand that must be triggered by a CRON.
+     * When used with a CRON, a scheduled task will be triggered at a recurring interval, it is your own responsibility to implement a
+     * logic of executing or not based on how long it's been since the last execution.
+     * Please look at the aws-scheduling module for an example of how to setup a CRON using AWS Event Bridge with Pristine's listeners.
      * @param scheduledTasks The scheduled tasks to run. All services with the tag ServiceDefinitionTagEnum.ScheduledTask will be automatically injected here.
      * @param logHandler The log handler to use to output logs.
      */
