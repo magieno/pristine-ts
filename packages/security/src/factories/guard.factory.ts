5696d98341f3d8e4a82a4ddeb6d4fcc25aa8414d
mathieugh <mathieu.girouxh@gmail.com>
2022-01-24 09:41:58 -0500

    - Docs for the guard factory

--- packages/security/src/factories/guard.factory.ts
+++ packages/security/src/factories/guard.factory.ts
@@ -1,15 +1,26 @@
 import {DependencyContainer, injectable} from "tsyringe";
 import {GuardContextInterface} from "../interfaces/guard-context.interface";
 import {GuardInterface} from "../interfaces/guard.interface";
-import {GuardDecoratorError} from "../errors/guard-decorator.error";
 import {GuardInstantiationError} from "../errors/guard-instantiation.error";
 
+/**
+ * The GuardFactory returns the proper instantiated guard.
+ */
 @injectable()
 export class GuardFactory {
+
+    /**
+     * This function takes the guard context and returns the proper instantiated guard.
+     * It also validates that the guard is valid (it implements the GuardInterface).
+     * @param guardContext The guard context that contains the guard and the options to use.
+     * @param container The dependency container from which to retrieve the instantiated guard.
+     */
     fromContext(guardContext: GuardContextInterface, container: DependencyContainer): GuardInterface {
         // Check if the guard needs to be instantiated
         let instantiatedGuard: GuardInterface = guardContext.guard as GuardInterface;
 
+        // If guardContext.guard  is a function, we resolve that function through the container.
+        // TODO: validate if this is good.
         if (typeof instantiatedGuard === 'function') {
             instantiatedGuard = container.resolve(instantiatedGuard);
         }
