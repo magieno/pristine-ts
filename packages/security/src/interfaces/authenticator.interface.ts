f4e3e43e3a0782265a7d2c00dec479a47a106c61
mathieugh <mathieu.girouxh@gmail.com>
2022-01-25 13:01:15 -0500

    - Docs for authenticator interface

--- packages/security/src/interfaces/authenticator.interface.ts
+++ packages/security/src/interfaces/authenticator.interface.ts
@@ -2,6 +2,15 @@ import {IdentityInterface} from "@pristine-ts/common";
 import {ContextAwareInterface} from "@pristine-ts/common";
 import {RequestInterface} from "@pristine-ts/common";
 
+/**
+ * The Authenticator Interface defines what an authenticator should implement.
+ * It extends the ContextAwareInterface.
+ */
 export interface AuthenticatorInterface extends ContextAwareInterface {
+    /**
+     * Authenticates the request by providing the identity making the request if it exists.
+     * @param request The request to authenticate.
+     * @returns {IdentityInterface | undefined} The Identity making the request if it exists
+     */
     authenticate(request: RequestInterface): Promise<IdentityInterface | undefined>;
 }
