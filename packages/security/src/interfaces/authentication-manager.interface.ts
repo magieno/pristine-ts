1ca8bf5478725208d401879640d53b47cc3d7002
mathieugh <mathieu.girouxh@gmail.com>
2022-01-25 13:00:42 -0500

    - Docs for authentication manager

--- packages/security/src/interfaces/authentication-manager.interface.ts
+++ packages/security/src/interfaces/authentication-manager.interface.ts
@@ -1,6 +1,17 @@
 import {DependencyContainer} from "tsyringe";
 import {IdentityInterface, RequestInterface} from "@pristine-ts/common";
 
+/**
+ * The Authentication Manager Interface defines what the authentication manager should implement.
+ * It facilitates mocking.
+ */
 export interface AuthenticationManagerInterface {
+    /**
+     * Authenticates a request.
+     * @param request The request.
+     * @param routeContext The context for the route.
+     * @param container The dependency container.
+     * @returns { IdentityInterface | undefined } The identity making the request if available. Otherwise undefined.
+     */
     authenticate(request: RequestInterface, routeContext: any, container: DependencyContainer): Promise<IdentityInterface | undefined>
 }
