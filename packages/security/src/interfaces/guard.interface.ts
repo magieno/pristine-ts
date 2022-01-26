1a0605f46d5c049190ae916982876bbcf3e4d0a8
mathieugh <mathieu.girouxh@gmail.com>
2022-01-26 11:03:32 -0500

    - Docs for guard interface

--- packages/security/src/interfaces/guard.interface.ts
+++ packages/security/src/interfaces/guard.interface.ts
@@ -2,10 +2,25 @@ import {ContextAwareInterface, RequestInterface} from "@pristine-ts/common";
 import {IdentityInterface} from "@pristine-ts/common";
 import {GuardContextInterface} from "./guard-context.interface";
 
+/**
+ * The Guard Interface defines what a guard should implement.
+ * It extends the ContextAwareInterface.
+ */
 export interface GuardInterface extends ContextAwareInterface {
+    /**
+     * The keyname for the guard.
+     */
     keyname: string;
 
+    /**
+     * The context for the guard to use.
+     */
     guardContext?: GuardContextInterface;
 
+    /**
+     * Returns whether the guard authorizes the request to access the route or not.
+     * @param request The request ot authorize.
+     * @param identity The identity making the request.
+     */
     isAuthorized(request: RequestInterface, identity?: IdentityInterface): Promise<boolean>;
 }
