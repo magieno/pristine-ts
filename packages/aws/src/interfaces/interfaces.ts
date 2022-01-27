53768941b36ab10027d81ad4f371863cb694c687
mathieugh <mathieu.girouxh@gmail.com>
2022-01-27 14:45:11 -0500

    - Create interface

--- packages/aws/src/interfaces/interfaces.ts
+++ packages/aws/src/interfaces/interfaces.ts
@@ -1,2 +1,5 @@
 export * from "./dynamodb-client.interface";
+export * from "./event-bridge-client.interface";
 export * from "./request-mapper.interface";
+export * from "./s3-client.interface";
+export * from "./sqs-client.interface";
