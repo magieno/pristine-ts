fbe3d8578dcf1ea1d0f17b5ec9366cd5c5189265
Etienne Noel <etienne.noel@ima-tech.ca>
2022-01-26 20:18:06 -0500

    Updated based on comments.

--- packages/core/src/kernel.ts
+++ packages/core/src/kernel.ts
@@ -18,6 +18,7 @@ import {CoreModuleKeyname} from "./core.module.keyname";
 import { v4 as uuidv4 } from 'uuid';
 import {ExecutionContextInterface} from "./interfaces/execution-context.interface";
 import {EventPipeline} from "./pipelines/event.pipeline";
+import {KernelInitializationError} from "./errors/kernel-initialization.error";
 
 /**
  * This is the central class that manages the lifecyle of this library.
@@ -70,6 +71,10 @@ export class Kernel {
 
         // Inits the application module and its dependencies.
         const initializedModuleSpans = await this.initModule(module);
+
+        if(this.initializationSpan === undefined) {
+            throw new KernelInitializationError("The InitializationSpan is undefined and shouldn't be.")
+        }
 
         // Add every spans as a child of the Initialization Span
         initializedModuleSpans.forEach(span => this.initializationSpan.addChild(span));
--- packages/networking/src/interceptors/response-headers.interceptor.ts
+++ packages/networking/src/interceptors/response-headers.interceptor.ts
@@ -6,7 +6,7 @@ import {MethodRouterNode} from "../nodes/method-router.node";
 import {RequestInterceptorInterface} from "../interfaces/request-interceptor.interface";
 
 /**
- * The RouterResponse Interceptor intercepts the response of the router by adding the response headers specified by the response header decorator.
+ * The Response Interceptor intercepts the response of the router by adding the response headers specified by the response header decorator.
  * It is tagged as a RequestInterceptor so it can be automatically injected with the all the other RequestInterceptor.
  */
 @tag(ServiceDefinitionTagEnum.RequestInterceptor)
