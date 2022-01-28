f684f377321bce8ca58f9af4535d3b70f807b28a
mathieugh <mathieu.girouxh@gmail.com>
2022-01-28 12:25:09 -0500

    - Fix s3 client interface

--- packages/aws/src/interfaces/s3-client.interface.ts
+++ packages/aws/src/interfaces/s3-client.interface.ts
@@ -1,7 +1,9 @@
+import { S3PresignedOperationTypeEnum } from "../enums/s3-presigned-operation-type.enum";
+
 export interface S3ClientInterface {
     get(bucketName: string, key: string): Promise<any>;
     listKeys(bucketName: string): Promise<string[]>;
     listObjects(bucketName: string): Promise<any[]>;
     upload(bucketName: string, key: string, data: any, contentEncoding?: string, contentType?: string): Promise<void>;
-    createSignedUrl(bucketName: string, key: string, operation: string): Promise<string>;
+    createSignedUrl(bucketName: string, key: string, operation: S3PresignedOperationTypeEnum, expiresIn: number): Promise<string>;
 }
