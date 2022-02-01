2ba09f4338cf2dde8a4630c569f7eeae95d17cdb
mathieugh <mathieu.girouxh@gmail.com>
2022-02-01 08:48:34 -0500

    - Docs default identity provider

--- packages/security/src/providers/default-identity.provider.ts
+++ packages/security/src/providers/default-identity.provider.ts
@@ -4,7 +4,9 @@ import {injectable} from "tsyringe";
 import {IdentityProviderInterface} from "../interfaces/identity-provider.interface";
 
 /**
+ * This default identity provider is here so that there is always at least one service tagged with IdentityProvider
  * Until there's a fix for: https://github.com/microsoft/tsyringe/issues/63
+ * It resolves the same identity it is passed.
  */
 @injectable()
 @tag(ServiceDefinitionTagEnum.IdentityProvider)
