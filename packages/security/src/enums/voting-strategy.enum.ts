b265bd31b700856155e0cd80db8c0306bfeb9e74
mathieugh <mathieu.girouxh@gmail.com>
2022-02-01 08:48:12 -0500

    - Docs each enum

--- packages/security/src/enums/voting-strategy.enum.ts
+++ packages/security/src/enums/voting-strategy.enum.ts
@@ -3,6 +3,13 @@
  * merge the results of all the voter.
  */
 export enum VotingStrategyEnum {
+    /**
+     * When all the voters abstain from voting, the access is granted.
+     */
     GrantOnUnanimousAbstention = "GRANT_ON_UNANIMOUS_ABSTENTION",
+
+    /**
+     * When all the voters abstain from voting, the access is denied.
+     */
     DenyOnUnanimousAbstention = "DENY_ON_UNANIMOUS_ABSTENTION"
 }
