f00f4c46baa27b7f70b7441cb64d601cc7594c42
mathieugh <mathieu.girouxh@gmail.com>
2022-02-03 10:02:53 -0500

    - Docs for enum

--- packages/stripe/src/enums/stripe-event-type.enum.ts
+++ packages/stripe/src/enums/stripe-event-type.enum.ts
@@ -1,5 +1,19 @@
+/**
+ * The event types that we handle from Stripe.
+ */
 export enum StripeEventTypeEnum {
+    /**
+     * When a subscription was created.
+     */
     StripeSubscriptionCreated = "STRIPE_SUBSCRIPTION_CREATED",
+
+    /**
+     * When a subscription was updated.
+     */
     StripeSubscriptionUpdated = "STRIPE_SUBSCRIPTION_UPDATED",
+
+    /**
+     * When a subscription was deleted.
+     */
     StripeSubscriptionDeleted = "STRIPE_SUBSCRIPTION_DELETED",
 }
