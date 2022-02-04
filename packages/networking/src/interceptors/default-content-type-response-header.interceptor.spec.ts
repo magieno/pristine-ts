a4ffa02c562955d5e75e6125ad1a98ee1be28083
mathieugh <mathieu.girouxh@gmail.com>
2022-02-04 13:28:05 -0500

    - Tests for default content type

---
+++ packages/networking/src/interceptors/default-content-type-response-header.interceptor.spec.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata"
+import {LogHandlerMock} from "../../../../tests/mocks/log.handler.mock";
+import { HttpMethod, Request, Response } from "@pristine-ts/common";
+import { DefaultContentTypeResponseHeaderInterceptor } from "./default-content-type-response-header.interceptor";
+
+describe("Default Content Type response header Interceptor", () => {
+    const logHandlerMock = new LogHandlerMock()
+
+    it("should return the unmodified response if the interceptor is not active", async () => {
+        const defaultContentTypeResponseHeaderInterceptor: DefaultContentTypeResponseHeaderInterceptor = new DefaultContentTypeResponseHeaderInterceptor("application/json", false, logHandlerMock);
+
+        const response = new Response();
+        const request = new Request(HttpMethod.Get, "http://localhost");
+
+        expect((await defaultContentTypeResponseHeaderInterceptor.interceptResponse(response, request)).hasHeader("Content-Type")).toBeFalsy();
+    })
+
+    it("should return the unmodified response if the Content-Type is already present in the Response", async () => {
+        const defaultContentTypeResponseHeaderInterceptor: DefaultContentTypeResponseHeaderInterceptor = new DefaultContentTypeResponseHeaderInterceptor("application/json", true, logHandlerMock);
+
+        const response = new Response();
+        const request = new Request(HttpMethod.Get, "http://localhost");
+        response.setHeader("Content-Type", "text/plain")
+
+        expect((await defaultContentTypeResponseHeaderInterceptor.interceptResponse(response, request)).getHeader("Content-Type")).toBe("text/plain");
+    })
+
+    it("should return the response with the header Content-Type set to application/json", async () => {
+        const defaultContentTypeResponseHeaderInterceptor: DefaultContentTypeResponseHeaderInterceptor = new DefaultContentTypeResponseHeaderInterceptor("application/json", true, logHandlerMock);
+
+        const response = new Response();
+        const request = new Request(HttpMethod.Get, "http://localhost");
+
+        expect((await defaultContentTypeResponseHeaderInterceptor.interceptResponse(response, request)).getHeader("Content-Type")).toBe("application/json");
+    })
+})
