502d4fa5d6dd1e83cfca3f6c62fe8e7436b55c4d
mathieugh <mathieu.girouxh@gmail.com>
2022-02-08 14:40:28 -0500

    - Doc interfaces

--- packages/telemetry/src/interfaces/tracer.interface.ts
+++ packages/telemetry/src/interfaces/tracer.interface.ts
@@ -1,9 +1,26 @@
-import {Span} from "../models/span.model";
 import {Readable} from "stream";
 
+/**
+ * This interface represents what a tracer should have.
+ */
 export interface TracerInterface {
+    /**
+     * The stream for when a span is started.
+     */
     spanStartedStream?: Readable;
+
+    /**
+     * The stream for when a span has ended.
+     */
     spanEndedStream?: Readable;
+
+    /**
+     * The stream for when a trace is started.
+     */
     traceStartedStream?: Readable;
+
+    /**
+     * The stream for when a trace has ended.
+     */
     traceEndedStream?: Readable;
 }
