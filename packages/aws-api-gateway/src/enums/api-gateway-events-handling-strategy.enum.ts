553c3f5e15857dd298e5e97993940e92aad19dbc
mathieugh <mathieu.girouxh@gmail.com>
2022-07-15 11:53:22 -0400

    Doc strategies

--- packages/aws-api-gateway/src/enums/api-gateway-events-handling-strategy.enum.ts
+++ packages/aws-api-gateway/src/enums/api-gateway-events-handling-strategy.enum.ts
@@ -1,4 +1,14 @@
+/**
+ * Handling strategies for Api gateway events
+ */
 export enum ApiGatewayEventsHandlingStrategyEnum {
+    /**
+     * Handle Api gateway as an Event.
+     */
     Event= "EVENT",
+
+    /**
+     * Handle Api gateway event as a request.
+     */
     Request = "REQUEST",
 }
