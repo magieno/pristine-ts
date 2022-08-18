3d85ec8543659efdb9dda4ce1fb77b756fb73f1b
mathieugh <mathieu.girouxh@gmail.com>
2022-08-18 14:39:01 -0400

    Doc base api mapper

--- packages/aws-api-gateway/src/mappers/base-api-event.mapper.ts
+++ packages/aws-api-gateway/src/mappers/base-api-event.mapper.ts
@@ -1,7 +1,15 @@
 import {HttpMethod} from "@pristine-ts/common";
 
+/**
+ * The base api event mapper from which more specific mapper will extend.
+ */
 export class BaseApiEventMapper {
 
+    /**
+     * Maps the http method to the corresponding enum value.
+     * @param method The http method of the request.
+     * @protected
+     */
     protected mapHttpMethod(method: string): HttpMethod {
         method = method.toLowerCase();
 
