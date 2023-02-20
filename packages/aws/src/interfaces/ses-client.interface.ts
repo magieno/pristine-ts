f95a98c557560f2775b2037d77b82eddb4385b41
Etienne Noel <etiennenoel@gmail.com>
2023-02-20 15:12:26 -0800

    Updated the comments.

--- packages/aws/src/interfaces/ses-client.interface.ts
+++ packages/aws/src/interfaces/ses-client.interface.ts
@@ -3,22 +3,30 @@ import {EmailModel} from "../models/email.model";
 import {SesMessageSentConfirmationModel} from "../models/ses-message-sent-confirmation.model";
 
 /**
- * The S3Client Interface defines the methods that an S3 client must implement.
- * When injecting the S3 client the 'S3ClientInterface' tag should be used.
+ * The SESClient Interface defines the methods that an SES client must implement.
+ * When injecting the SES client the 'SesClientInterface' tag should be used.
  */
 export interface SesClientInterface {
     /**
-     * Returns the instantiated SQSClient from the @aws-sdk/client-sqs library.
-     * @param endpoint The endpoint for which the SQS client is created.
+     * Returns the instantiated SESClient from the @aws-sdk/client-sqs library.
+     * @param endpoint The endpoint for which the SES client is created.
      */
     getClient(endpoint?: string): SESClient;
 
     /**
-     * Sends a message to the specified Queue URL.
+     * This sends an email.
+     *
      * @param email
      * @param endpoint
      */
     send(email: EmailModel, endpoint?: string): Promise<SesMessageSentConfirmationModel>
 
+    /**
+     * This sends an email with the specified template and template Data.
+     * @param email
+     * @param templateName
+     * @param templateData
+     * @param endpoint
+     */
     sendTemplate(email: EmailModel, templateName: string, templateData: {[key in string]: string}, endpoint?: string): Promise<SesMessageSentConfirmationModel>
 }
