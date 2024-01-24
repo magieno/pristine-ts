7555e9c3ccf0bec12e1213d5bb9bf825d2d81bc1
Etienne Noel <etiennenoel@gmail.com>
2024-01-23 20:28:28 -0800

    - Added a @bodyMapping decorator that accepts either a ClassType, a function or a DataMapperBuilder.

--- packages/data-mapping/src/data-mapping.module.ts
+++ packages/data-mapping/src/data-mapping.module.ts
@@ -3,6 +3,7 @@ import {LoggingModule} from "@pristine-ts/logging";
 import {DataMappingModuleKeyname} from "./data-mapping.module.keyname";
 
 export * from "./builders/builders";
+export * from "./decorators/decorators";
 export * from "./enums/enums";
 export * from "./errors/errors";
 export * from "./interceptors/interceptors";
@@ -11,6 +12,7 @@ export * from "./mappers/mappers";
 export * from "./nodes/nodes";
 export * from "./normalizer-options/normalizer-options";
 export * from "./normalizers/normalizers";
+export * from "./request-interceptors/request-interceptors";
 export * from "./types/types";
 
 export const DataMappingModule: ModuleInterface = {
