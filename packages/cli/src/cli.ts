c7be77a2499c2de35051b35ac0c2ef0e6dba1b2a
Etienne Noel <etiennenoel@gmail.com>
2024-03-08 12:54:31 -0800

    - in progress.

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,10 +1,10 @@
-import {AppModuleInterface} from "@pristine-ts/common";
 import {ExecutionContextKeynameEnum, Kernel} from "@pristine-ts/core";
 import fs from "fs";
 import {LoggingModuleKeyname, OutputModeEnum, SeverityEnum} from "@pristine-ts/logging";
+import {DirectoryManager, MatchTypeEnum} from "@pristine-ts/file";
 
-const getLocalAppModuleCJSPath = async (): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
+const getLocalAppModuleCJSPath = async (): Promise<string | undefined> => {
+    return new Promise<string | undefined>((resolve, reject) => {
         const packageJson = process.cwd() + "/package.json";
 
         fs.stat(packageJson, async (err, stats) => {
@@ -12,7 +12,7 @@ const getLocalAppModuleCJSPath = async (): Promise<string> => {
                 const message = "Cannot find the package.json at '" + packageJson + "'. Make sure that you execute this script directly from the parent of your 'package.json'"
                 console.error(message);
                 console.error(err);
-                throw new Error(message);
+                return resolve(undefined);
             }
 
             const {pristine} = await import(packageJson);
@@ -20,18 +20,18 @@ const getLocalAppModuleCJSPath = async (): Promise<string> => {
             if(pristine === undefined) {
                 const message = "There needs to be a section 'pristine' in your package.json file located at: '" + packageJson + "'";
                 console.error(message);
-                throw new Error(message);
+                return resolve(undefined);
             }
 
             if(pristine.appModule === undefined) {
                 const message = "There should be a section 'appModule' under 'pristine' in your package.json file located at: '" + packageJson + "'"
                 console.error(message);
-                throw new Error(message);
+                return resolve(undefined);
             }
             if(pristine.appModule.cjsPath === undefined) {
                 const message = "There should be a property 'cjsPath' under 'pristine.appModule' in your package.json file located at: '" + packageJson + "'"
                 console.error(message);
-                throw new Error(message);
+                return resolve(undefined);
             }
 
             return resolve( process.cwd() + "/" + pristine.appModule.cjsPath);
@@ -39,9 +39,23 @@ const getLocalAppModuleCJSPath = async (): Promise<string> => {
     })
 }
 
-const bootstrap = async () => {
 
+
+const bootstrap = async () => {
     const cjsPath = await getLocalAppModuleCJSPath();
+
+    if(cjsPath === undefined) {
+        // Try the automatic Module creation.
+
+        // Loop over the `node_modules/@pristine-ts/*/dist/lib/cjs/*.module.js` folders and find all the module files that are loaded.
+        const directoryManager = new DirectoryManager();
+        const directories = await directoryManager.list(process.cwd() + "/node_modules/@pristine-ts", {
+            matchType: MatchTypeEnum.Filename,
+            match: "*.module.js",
+        });
+
+        return;
+    }
 
     const localAppModule = await import(cjsPath);
 
