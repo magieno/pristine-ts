d4afbffaedc1dc95d8e9a1fe66cb2f1653326af5
Etienne Noel <etiennenoel@gmail.com>
2024-03-20 12:44:08 -0700

    update

--- packages/cli/src/options/console-readline.options.ts
+++ packages/cli/src/options/console-readline.options.ts
@@ -1,7 +1,7 @@
 export class ConsoleReadlineOptions {
     showCharactersOnTyping: boolean = true;
 
-    constructor(options: Partial<ConsoleReadlineOptions>) {
-        this.showCharactersOnTyping = options.showCharactersOnTyping ?? this.showCharactersOnTyping;
+    constructor(options?: Partial<ConsoleReadlineOptions>) {
+        this.showCharactersOnTyping = options?.showCharactersOnTyping ?? this.showCharactersOnTyping;
     }
 }
