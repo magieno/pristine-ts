a2268c58fcb522de576bbbcbca23581f74953aff
Etienne Noel <etiennenoel@gmail.com>
2024-04-15 10:59:57 -0700

    - Added test for the regex

--- packages/file/src/enums/match-type.enum.ts
+++ packages/file/src/enums/match-type.enum.ts
@@ -2,5 +2,5 @@ export enum MatchTypeEnum {
     Base = "BASE", // Means you're matching the base filename without the extension and without the path such as "results"
     Extension = "EXT", // Means you're matching file extensions (without leading ".")  such as "jpeg"
     Filename = "FILE_NAME", // Means you're matching the base filename and extension  such as "results.jpeg"
-    Path = "PATH",
+    Path = "PATH", // Means you're matching the full path such as "/home/user/results.jpeg"
 }
--- packages/file/src/managers/file.manager.spec.ts
+++ packages/file/src/managers/file.manager.spec.ts
@@ -19,4 +19,47 @@ describe("File Manager", () => {
         expect(cursors[3].line).toBe(10);
         expect(cursors[3].position).toBe(51);
     })
+    it("should properly find the elements in the text when a regex is passed", async () => {
+        const fileManager = new FileManager();
+
+        const cursors = await fileManager.findInFile(/(eleifend)|(pulvinar)/gi, "test-files/lorem_ipsum.txt");
+
+        expect(cursors.length).toBe(9);
+
+        // eleifend
+        expect(cursors[0].line).toBe(2);
+        expect(cursors[0].position).toBe(416);
+
+        // pulvinar
+        expect(cursors[1].line).toBe(2);
+        expect(cursors[1].position).toBe(457);
+
+        // pulvinar
+        expect(cursors[2].line).toBe(6);
+        expect(cursors[2].position).toBe(24);
+
+        // eleifend
+        expect(cursors[3].line).toBe(6);
+        expect(cursors[3].position).toBe(33);
+
+        // pulvinar
+        expect(cursors[4].line).toBe(8);
+        expect(cursors[4].position).toBe(213);
+
+        // eleifend
+        expect(cursors[5].line).toBe(8);
+        expect(cursors[5].position).toBe(595);
+
+        // pulvinar
+        expect(cursors[6].line).toBe(10);
+        expect(cursors[6].position).toBe(332);
+
+        // pulvinar
+        expect(cursors[7].line).toBe(12);
+        expect(cursors[7].position).toBe(76);
+
+        // pulvinar
+        expect(cursors[8].line).toBe(14);
+        expect(cursors[8].position).toBe(414);
+    })
 })
