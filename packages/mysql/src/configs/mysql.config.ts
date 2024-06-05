a8507789010409d32e0da355d5ee99266119f640
Etienne Noel <etiennenoel@gmail.com>
2024-06-05 15:40:44 -0700

    - Fix issue

--- packages/mysql/src/configs/mysql.config.ts
+++ packages/mysql/src/configs/mysql.config.ts
@@ -1,18 +1,10 @@
-import {injectable} from "tsyringe";
-import {ServiceDefinitionTagEnum, tag} from "@pristine-ts/common";
-
-@tag(ServiceDefinitionTagEnum.MysqlConfig)
-@injectable()
-export class MysqlConfig {
-    constructor(
-                public readonly uniqueKeyname: string,
-                public readonly host: string,
-                public readonly port: number,
-                public readonly user: string,
-                public readonly password: string,
-                public readonly connectionLimit: number,
-                public readonly debug: boolean,
-                public readonly database: string,
-                ) {
-    }
+export interface MysqlConfig {
+    uniqueKeyname: string,
+    host: string,
+    port: number,
+    user: string,
+    password: string,
+    connectionLimit: number,
+    debug: boolean,
+    database: string,
 }
