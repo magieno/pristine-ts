75100ae0ebec43bce6c0fe8762a7188ad07be5e6
Etienne Noel <etiennenoel@gmail.com>
2024-09-24 14:42:36 -0700

    - Adding a trailing slash to see if it still works.

--- tests/e2e/src/controllers/nested.controller.ts
+++ tests/e2e/src/controllers/nested.controller.ts
@@ -4,12 +4,12 @@ import {HttpMethod} from "@pristine-ts/common";
 
 @controller("/api/2.0/magieno/pristine")
 export class NestedController {
-    @route(HttpMethod.Get, "")
+    @route(HttpMethod.Get, "/")
     get() {
         return {"NestedController": true}
     }
 
-    @route(HttpMethod.Post, "")
+    @route(HttpMethod.Post, "/")
     post(@body() body: any) {
         return body;
     }
