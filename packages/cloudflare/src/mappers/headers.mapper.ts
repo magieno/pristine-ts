d4ba07a8b11586d73c9ce261fd7c541be89e8288
Etienne Noel <etiennenoel@gmail.com>
2025-11-28 13:30:00 -0800

    update

--- packages/cloudflare/src/mappers/headers.mapper.ts
+++ packages/cloudflare/src/mappers/headers.mapper.ts
@@ -1,28 +1,28 @@
 import {injectable} from "tsyringe";
 
-@injectable()
-export class HeadersMapper {
-  map(headers: Headers): { [key: string]: string } {
-    const mappedHeaders: { [key: string]: string } = {}
-
-    headers.forEach((key: string, value: string) => {
-      mappedHeaders[key] = value;
-    })
-
-    return mappedHeaders;
-  }
-
-  reverseMap(headers: { [key: string]: string }): Headers {
-    const mappedHeaders = new Headers();
-
-    for (const key in headers) {
-      if (headers.hasOwnProperty(key) === false) {
-        continue;
-      }
-
-      mappedHeaders.set(key, headers[key]);
-    }
-
-    return mappedHeaders;
-  }
-}
+// @injectable()
+// export class HeadersMapper {
+//   map(headers: Headers): { [key: string]: string } {
+//     const mappedHeaders: { [key: string]: string } = {}
+//
+//     headers.forEach((key: string, value: string) => {
+//       mappedHeaders[key] = value;
+//     })
+//
+//     return mappedHeaders;
+//   }
+//
+//   reverseMap(headers: { [key: string]: string }): Headers {
+//     const mappedHeaders = new Headers();
+//
+//     for (const key in headers) {
+//       if (headers.hasOwnProperty(key) === false) {
+//         continue;
+//       }
+//
+//       mappedHeaders.set(key, headers[key]);
+//     }
+//
+//     return mappedHeaders;
+//   }
+// }
--- packages/cloudflare/src/mappers/response.mapper.ts
+++ packages/cloudflare/src/mappers/response.mapper.ts
@@ -1,16 +1,16 @@
 import {injectable} from "tsyringe";
-import {Response as PristineResponse} from "@pristine-ts/common";
-import {HeadersMapper} from "./headers.mapper";
-
-@injectable()
-export class ResponseMapper {
-  constructor(private readonly headersMapper: HeadersMapper) {
-  }
-
-  map(pristineResponse: PristineResponse): Response {
-    return new Response(pristineResponse.body, {
-      status: pristineResponse.status,
-      headers: this.headersMapper.reverseMap(pristineResponse.headers ?? {}),
-    });
-  }
-}
+// import {Response as PristineResponse} from "@pristine-ts/common";
+// import {HeadersMapper} from "./headers.mapper";
+//
+// @injectable()
+// export class ResponseMapper {
+//   constructor(private readonly headersMapper: HeadersMapper) {
+//   }
+//
+//   map(pristineResponse: PristineResponse): Response {
+//     return new Response(pristineResponse.body, {
+//       status: pristineResponse.status,
+//       headers: this.headersMapper.reverseMap(pristineResponse.headers ?? {}),
+//     });
+//   }
+// }
